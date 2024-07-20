230802a1606e47baff512fc47bff2facd6e29f58
Adam Langmesser <s27119@pjwstk.edu.pl>
2024-07-20 13:53:22 +0200

    Testing the test runner pipeline

--- backend/account/src/main/java/com/merkury/account/user/User.java
+++ backend/account/src/main/java/com/merkury/account/user/User.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "\"user\"")
 @Getter
 @Builder
 @NoArgsConstructor
@@ -17,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     private String name;
     private String surname;
     private String email;
