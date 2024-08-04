287e9ec3e88ed99dd1267ecec16f4d083806b742
Mredosz <mredosz4@gmail.com>
2024-08-04 17:45:25 +0200

    Prettier test

--- venus/src/App.jsx
+++ venus/src/App.jsx
@@ -1,10 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-      <></>
+    <>
+      <div>
+        <h1></h1>
+        <ul>
+          <li> </li>
+        </ul>
+      </div>
+    </>
   );
 }
 
-export default App
+export default App;
