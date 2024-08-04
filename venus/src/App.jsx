64c38bca7f888054c05a3a23084a85c190deab15
Mredosz <mredosz4@gmail.com>
2024-08-04 19:42:52 +0200

    add component

--- venus/src/App.jsx
+++ venus/src/App.jsx
@@ -1,15 +1,8 @@
-import "./App.css";
+import Registration from "./account/components/Registration.jsx";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1></h1>
-        <ul>
-          <li> </li>
-        </ul>
-      </div>
-    </>
+      <Registration/>
   );
 }
 
---
+++ venus/src/account/components/Registration.jsx
@@ -0,0 +1,5 @@
+export default function Registration (){
+    return (
+        <h1 className='text-center bg-amber-400'>Hello</h1>
+    );
+}
