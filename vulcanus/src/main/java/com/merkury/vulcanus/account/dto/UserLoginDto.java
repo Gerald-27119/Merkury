3e5c75aee6ffd7d0be2f41e64e15c8a54677abd7
Halvah <bart.bohatyrewicz@gmail.com>
2024-08-08 22:08:10 +0200

    config file repair

--- vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserLoginDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserLoginDto.java
@@ -9,5 +9,5 @@ public record UserLoginDto(@NotBlank(message = "Username can't be empty.")
                            String username,
                            @NotBlank(message = "Password can't be empty.")
                            String password) {
-
+//aaa
 }
