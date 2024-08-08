ec2c67835c7208e32692da302d03d91fa03b9b8e
Halvah <bart.bohatyrewicz@gmail.com>
2024-08-08 21:22:32 +0200

    added branch, added jakarta annotation validation for login and register dto

--- vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserLoginDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserLoginDto.java
@@ -1,6 +1,13 @@
 package com.merkury.vulcanus.account.dto;
 
 
-public record UserLoginDto(String username, String password) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginDto(@NotBlank(message = "Username can't be empty.")
+                           String username,
+                           @NotBlank(message = "Password can't be empty.")
+                           String password) {
 
 }
--- vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserRegisterDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserRegisterDto.java
@@ -1,5 +1,18 @@
 package com.merkury.vulcanus.account.dto;
 
-public record UserRegisterDto(String username, String email, String password) {
+import jakarta.validation.constraints.*;
+
+public record UserRegisterDto(@NotBlank(message = "Username can't be empty.")
+                              @Pattern(regexp="/^[a-zA-Z0-9_]$/", message="Unallowed characters.")
+                              @Size(min=3,max=16,message="Username must contain 3 to 16 characters.")
+                              String username,
+                              @NotBlank(message = "Email can't be empty.")
+                              @Size(max=100, message="Email too long.")
+                              @Email(message="This is not a valid email address.")
+                              String email,
+                              @NotBlank(message = "Password can't be empty.")
+                              @Pattern(regexp="/^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\s:])(\\S)$/", message="Password must contain a small and big letter, a number and a special character.")
+                              @Size(min=8,max=16, message="Password length must be between 8 to 16 characters.")
+                              String password) {
 
 }
