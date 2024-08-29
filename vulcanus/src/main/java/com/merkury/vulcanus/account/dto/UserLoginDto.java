e1681a1eb3023c3fe782bb411e8688478e692da6
Halvah <67759169+Halvah@users.noreply.github.com>
2024-08-29 21:52:35 +0200

    feature/MER-88 (#26)
    * added branch, added jakarta annotation validation for login, register and password reset dto

--- vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserLoginDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/account/dto/UserLoginDto.java
@@ -1,6 +1,10 @@
 package com.merkury.vulcanus.account.dto;
 
 
-public record UserLoginDto(String username, String password) {
+import jakarta.validation.constraints.NotBlank;
 
+public record UserLoginDto(@NotBlank
+                           String username,
+                           @NotBlank
+                           String password) {
 }
