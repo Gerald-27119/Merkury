dcb3b7249349027dedb5935eb628e007e0ab55b5
Adam Langmesser <114993042+Gerald-27119@users.noreply.github.com>
2024-09-11 17:56:03 +0200

    feature/MER-28 (#29)
    * Update of gitignore
    
    * Cornerstone for use of docker.
    
    * Update of gitignore to exclude dataSources.xml
    
    * Connecting spring with both mongo and postgres
    
    * Successful creation of two containers (mongo and postgres) for local development
    
    * Correction of CI/CD to include docker
    
    * Correction of CI/CD to include docker
    
    * poprawki CI/CD

---
+++ vulcanus/docker/postgres-init.sql
@@ -0,0 +1,18 @@
+-- CREATE TABLE IF NOT EXISTS user_entity
+-- (
+--     id                      SERIAL PRIMARY KEY,
+--     email                   VARCHAR(255) UNIQUE NOT NULL,
+--     username                VARCHAR(255) UNIQUE NOT NULL,
+--     password                VARCHAR(255)        NOT NULL,
+--     role                    VARCHAR(50)         NOT NULL,
+--     account_non_expired     BOOLEAN DEFAULT TRUE,
+--     account_non_locked      BOOLEAN DEFAULT TRUE,
+--     credentials_non_expired BOOLEAN DEFAULT TRUE,
+--     enabled                 BOOLEAN DEFAULT TRUE
+-- );
+--
+-- -- Insert initial data
+-- INSERT INTO user_entity (email, username, password, role, account_non_expired, account_non_locked,
+--                          credentials_non_expired, enabled)
+-- VALUES ('admin@example.com', 'admin', 'password_encoded_string', 'ADMIN', TRUE, TRUE, TRUE, TRUE),
+--        ('user@example.com', 'user', 'password_encoded_string', 'USER', TRUE, TRUE, TRUE, TRUE);
