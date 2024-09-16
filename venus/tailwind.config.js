05fe7cd02297649358933c6ef2a3d869663300b6
Halvah <bart.bohatyrewicz@gmail.com>
2024-09-16 15:26:15 +0200

    main site + logout button + button comp

--- venus/tailwind.config.js
+++ venus/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      height: {
+        "1/10": "10%",
+      },
       colors: {
         gold: "#D4AF37",
         basalt: "#424448",
