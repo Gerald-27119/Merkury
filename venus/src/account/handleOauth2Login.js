2e82866342e1d051105e0f1c78988a7226439f31
stanoz <s.oziemczuk@gmail.com>
2024-09-23 10:17:49 +0200

    implement errors handling

--- venus/src/account/handleOauth2Login.js
+++ venus/src/account/handleOauth2Login.js
@@ -1,10 +1,10 @@
 import axios from "axios";
+import { json } from "react-router-dom";
 
 export default async function handleOauth2Login(errorMessage, url) {
-  try {
-    const response = await axios.get(url);
-    window.location.href = response.data.redirectUrl;
-  } catch (error) {
-    console.error(errorMessage, error);
+  const response = await axios.get(url);
+  if (response.status !== 201) {
+    throw json({ message: response.message }, { status: response.status });
   }
+  window.location.href = "/temp";
 }
