6868b97059c03972d1c519e6a4fa09e4f81c9c92
Adam Langmesser <s27119@pjwstk.edu.pl>
2024-10-27 19:26:25 +0100

    MER-128 implementacja Leafleta

--- venus/src/router/router-config.jsx
+++ venus/src/router/router-config.jsx
@@ -6,6 +6,7 @@ import Registration from "../account/components/Registration.jsx";
 import Login from "../account/components/Login.jsx";
 import ForgotPassword from "../account/components/ForgotPassword.jsx";
 import NewPassword from "../account/components/newPassword.jsx";
+import Map from "../map/Map.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,7 +14,16 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       { index: true, element: <WelcomePage /> },
-      { path: "main-view", element: <MainView /> },
+      {
+        path: "main-view",
+        element: <MainView />,
+        children: [
+          {
+            index: true,
+            element: <Map />,
+          },
+        ],
+      },
       { path: "register", element: <Registration /> },
       { path: "login", element: <Login /> },
       { path: "/login/forgot-password", element: <ForgotPassword /> },
