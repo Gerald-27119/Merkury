5077e1f3b05dddc30e2b9410494d59ef8fcbe6b6
stanoz <s.oziemczuk@gmail.com>
2024-11-10 13:08:15 +0100

    refactor button for login and register, and removal of unused and empty Marker component

--- venus/src/pages/account/Account.jsx
+++ venus/src/pages/account/Account.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import Button from "./Button.jsx";
 
 export default function Account() {
+  const buttonClasses = "text-4xl p-3";
   return (
     <div className="background-image flex h-screen w-full items-center justify-center space-x-8">
       <Link to="/login">
-        <Button>Login</Button>
+        <Button classNames={buttonClasses}>Login</Button>
       </Link>
       <Link to="/register">
-        <Button>Register</Button>
+        <Button classNames={buttonClasses}>Register</Button>
       </Link>
     </div>
   );
