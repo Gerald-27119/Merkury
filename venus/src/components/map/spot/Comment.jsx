e647d43eefbcaff1d70fb59662cd36bc3a0a03e6
stanoz <s.oziemczuk@gmail.com>
2024-11-27 15:49:52 +0100

    implement displaying stars as rating in single comment

--- venus/src/components/map/spot/Comment.jsx
+++ venus/src/components/map/spot/Comment.jsx
@@ -1,7 +1,29 @@
 import { IoStarHalf } from "react-icons/io5";
 import { IoStar } from "react-icons/io5";
+import { IoIosStarOutline } from "react-icons/io";
 
 export default function Comment({ comment }) {
+  const calculateStars = (rating) => {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating - fullStars > 0;
+    let stars = [];
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<IoStar className="text-yellow-400" />);
+    }
+    if (hasHalfStar) {
+      stars.push(<IoStarHalf className="text-yellow-400" />);
+    }
+
+    if (stars.length < 5) {
+      const starsToAdd = 5 - stars.length;
+      for (let i = 0; i < starsToAdd; i++) {
+        stars.push(<IoIosStarOutline />);
+      }
+    }
+
+    return stars;
+  };
+
   return (
     <div className="border border-stone-400 rounded-sm m-1 p-1">
       <div className="flex mb-1">
@@ -9,14 +31,7 @@ export default function Comment({ comment }) {
           <div className="text-xs">{comment.publishDate}</div>
           <div className="text-base">{comment.author}</div>
         </div>
-        <div className="inline-flex">
-          {/*{comment.rating}*/}
-          <IoStar />
-          <IoStar />
-          <IoStar />
-          <IoStar />
-          <IoStarHalf />
-        </div>
+        <div className="inline-flex">{calculateStars(comment.rating)}</div>
       </div>
       <div>{comment.text}</div>
     </div>
