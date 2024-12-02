9c9382bec34a78c6552480952b23b14b2f291b5d
stanoz <s.oziemczuk@gmail.com>
2024-12-02 16:59:37 +0100

    implemented basics of photo gallery

--- venus/src/components/map/spot/PhotoGallery.jsx
+++ venus/src/components/map/spot/PhotoGallery.jsx
@@ -1,40 +1,66 @@
 import { RiArrowLeftWideLine } from "react-icons/ri";
 import { RiArrowRightWideLine } from "react-icons/ri";
 import Photo from "./Photo.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-let currentPhotoIndex = 0;
 export default function PhotoGallery({ photos }) {
+  const disabledBtnClasses = "bg-gray-300 border border-red-600 text-zinc-950";
+  const activeBtnClasses =
+    "bg-neutral-600 text-slate-50 cursor-pointer hover:bg-neutral-400 hover:text-zinc-950";
+
   const numberOfPhotos = photos.length;
-  const [currentPhoto, setCurrentPhoto] = useState(photos[currentPhotoIndex]);
+
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+  const [disableButton, setDisableButton] = useState({
+    previousBtn: true,
+    nextBtn: false,
+  });
+
+  useEffect(() => {
+    setDisableButton({
+      previousBtn: currentPhotoIndex === 0,
+      nextBtn: currentPhotoIndex === numberOfPhotos - 1,
+    });
+  }, [currentPhotoIndex]);
 
   const handleNextPhoto = () => {
-    if (currentPhotoIndex < numberOfPhotos) {
-      currentPhotoIndex++;
-      setCurrentPhoto(photos[currentPhotoIndex]);
+    if (currentPhotoIndex < numberOfPhotos - 1) {
+      setCurrentPhotoIndex((prevState) => prevState + 1);
     }
   };
 
   const handlePreviousPhoto = () => {
     if (currentPhotoIndex > 0) {
-      currentPhotoIndex--;
-      setCurrentPhoto(photos[currentPhotoIndex]);
+      setCurrentPhotoIndex((prevState) => prevState - 1);
     }
   };
 
   return (
     <>
-      {/*items-stretch w rodzicu*/}
       {(photos && photos.length) > 0 ? (
         <div className="flex justify-center items-stretch border m-1">
-          <div>
-            <RiArrowLeftWideLine size={15} onClick={handlePreviousPhoto} />
+          <div className="flex items-center  min-h-full bg-gray-950">
+            <RiArrowLeftWideLine
+              size={20}
+              onClick={handlePreviousPhoto}
+              className={
+                disableButton.previousBtn
+                  ? disabledBtnClasses
+                  : activeBtnClasses
+              }
+            />
           </div>
           <div>
-            <Photo photo={currentPhoto} />
+            <Photo photo={photos[currentPhotoIndex]} />
           </div>
           <div className="flex items-center  min-h-full bg-gray-950">
-            <RiArrowRightWideLine size={15} onClick={handleNextPhoto} />
+            <RiArrowRightWideLine
+              size={20}
+              onClick={handleNextPhoto}
+              className={
+                disableButton.nextBtn ? disabledBtnClasses : activeBtnClasses
+              }
+            />
           </div>
         </div>
       ) : (
