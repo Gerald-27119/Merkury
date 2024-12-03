3bc063e4371209f5cee8966d3f130a9f52b0ab69
KacperBadek <k.badek2001@gmail.com>
2024-12-03 18:02:31 +0100

    MER-141 quick-fix

--- vulcanus/src/main/java/com/merkury/vulcanus/VulcanusApplication.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/VulcanusApplication.java
@@ -3,11 +3,13 @@ package com.merkury.vulcanus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableAsync
+@EnableRetry
 public class VulcanusApplication {
 
     public static void main(String[] args) {
