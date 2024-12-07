c1c698c94792571ad8fef29ed0593f96d3f254d9
stanoz <s.oziemczuk@gmail.com>
2024-12-07 17:58:40 +0100

    working on weather component

--- venus/src/components/map/Weather.jsx
+++ venus/src/components/map/Weather.jsx
@@ -2,6 +2,8 @@ import { IoSunny } from "react-icons/io5";
 import { FiSunset } from "react-icons/fi";
 import { FiSunrise } from "react-icons/fi";
 import { RiArrowDownWideFill } from "react-icons/ri";
+import { WiHumidity } from "react-icons/wi";
+import { WiThermometer } from "react-icons/wi";
 import { useState } from "react";
 
 export default function Weather({ weather }) {
@@ -15,19 +17,32 @@ export default function Weather({ weather }) {
             {weather.general === "sunny" && (
               <IoSunny size={40} className="text-yellow-300 ml-3" />
             )}
-            <div className="text-xl text-sky-50 right-0 mr-5">
-              {weather.temperature}&deg;C
+            <div className="right-0 mr-5 flex items-center">
+              <WiThermometer size={30} className="text-red-400" />
+              <div className="text-xl text-white">
+                {weather.temperature}&deg;C
+              </div>
             </div>
           </div>
           {showDetails && (
-            <div>
-              <div className="flex">
-                <FiSunrise />
-                {weather.sunrise}
+            <div className="flex flex-col space-y-3 mt-3 ml-3">
+              <div className="flex items-center text-xl space-x-3">
+                <FiSunrise
+                  size={40}
+                  className="bg-gradient-to-b from-pink-500 via-red-500 to-orange-500 p-1 rounded-md"
+                />
+                <div>{weather.sunrise}</div>
               </div>
-              <div className="flex">
-                <FiSunset />
-                {weather.sunset}
+              <div className="flex items-center text-xl space-x-3">
+                <FiSunset
+                  size={40}
+                  className="bg-gradient-to-b from-red-500 via-orange-500 to-yellow-500 p-1 rounded-md"
+                />
+                <div>{weather.sunset} </div>
+              </div>
+              <div className="flex items-center text-xl">
+                <WiHumidity size={40} className="text-blue-500" />
+                {weather.humidity}%
               </div>
             </div>
           )}
