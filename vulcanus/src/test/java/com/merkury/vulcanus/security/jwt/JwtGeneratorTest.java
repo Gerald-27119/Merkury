02ac80b187af149bdf3006387cf1d8acd176d607
Adam Langmesser <s27119@pjwstk.edu.pl>
2024-12-24 13:20:23 +0100

    MER-153 przykładowe testy

---
+++ vulcanus/src/test/java/com/merkury/vulcanus/security/jwt/JwtGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.merkury.vulcanus.security.jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtGeneratorTest {
+
+    @Test
+    void generateToken() {
+    }
+}
---
+++ vulcanus/src/test/java/com/merkury/vulcanus/security/jwt/JwtManagerTest.java
@@ -0,0 +1,32 @@
+package com.merkury.vulcanus.security.jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtManagerTest {
+
+    @Test
+    void getUsernameFromJWT() {
+    }
+
+    @Test
+    void validateToken() {
+    }
+
+    @Test
+    void getExpirationDateFromToken() {
+    }
+
+    @Test
+    void isNotTokenExpired() {
+    }
+
+    @Test
+    void addTokenToCookie() {
+    }
+
+    @Test
+    void getJWTFromCookie() {
+    }
+}
