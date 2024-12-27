3081a14054d5c705ae6e6c5ed60baaab650b89c7
KacperBadek <40339146+KacperBadek@users.noreply.github.com>
2024-12-27 22:54:06 +0100

    MER-145 (#48)
    * MER-145 email customization, added templates for registration and password reset emails.
    
    * MER-145 added <style> to email templates so it can be put to css inliner
    
    * MER-145 added seperate files for css inliner
    
    * MER-145 changed directory name
    
    * MER-145 added enums: EmailTemplate, EmailTitle, EmailVariable. Added EmailData class. Deleted unused functions, fields.

--- vulcanus/src/main/java/com/merkury/vulcanus/controllers/AccountController.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/controllers/AccountController.java
@@ -16,6 +16,10 @@ import com.merkury.vulcanus.features.password.reset.PasswordResetTokenService;
 import com.merkury.vulcanus.model.entities.PasswordResetToken;
 import com.merkury.vulcanus.model.entities.UserEntity;
 import com.merkury.vulcanus.features.email.EmailService;
+import com.merkury.vulcanus.model.enums.EmailTemplate;
+import com.merkury.vulcanus.model.enums.EmailTitle;
+import com.merkury.vulcanus.model.enums.EmailVariable;
+import com.merkury.vulcanus.model.support.classes.EmailData;
 import com.merkury.vulcanus.observability.counter.invocations.InvocationsCounter;
 import jakarta.servlet.http.HttpServletResponse;
 import com.merkury.vulcanus.config.properties.UrlsProperties;
@@ -27,6 +31,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Map;
 
 /**
  * <h1>We are using HS256 algorithm to sign the JWT token (for now).</h1>
@@ -41,8 +47,6 @@ public class AccountController {
     private final AccountService accountService;
     private final EmailService emailService;
     private final UrlsProperties urlsProperties;
-    private final String USER_REGISTERED_MESSAGE = "Thank you for registering in our service!\nYour account is now active.";
-    private final String USER_REGISTERED_TITLE = "Register confirmation";
     private final PasswordResetTokenService passwordResetTokenService;
     private final RestartPasswordService restartPasswordService;
 
@@ -62,8 +66,15 @@ public class AccountController {
         accountService.registerUser(userRegisterDto);
         log.info("User registered successfully!");
 
+        EmailData emailData = EmailData.builder()
+                .receiver(userRegisterDto.email())
+                .title(EmailTitle.USER_REGISTERED.getTitle())
+                .template(EmailTemplate.REGISTRATION.getTemplateName())
+                .variables(Map.of(EmailVariable.USERNAME.getVariable(), userRegisterDto.username()))
+                .build();
+
         log.info("Sending email...");
-        emailService.sendEmail(userRegisterDto.email(), USER_REGISTERED_TITLE, USER_REGISTERED_MESSAGE);
+        emailService.sendEmail(emailData);
 
         var user = new UserLoginDto(userRegisterDto.username(), userRegisterDto.password());
         log.info("Start handling logging in user");
@@ -103,9 +114,17 @@ public class AccountController {
 
         var userEmail = loginResponseDto.userEmail();
         if (loginResponseDto.isUserRegistered()) {
+            String username = restartPasswordService.getUserByEmail(userEmail).getUsername();
+
+            EmailData emailData = EmailData.builder()
+                    .receiver(userEmail)
+                    .title(EmailTitle.USER_REGISTERED.getTitle())
+                    .template(EmailTemplate.REGISTRATION.getTemplateName())
+                    .variables(Map.of(EmailVariable.USERNAME.getVariable(), username))
+                    .build();
+
             log.info("Sending email...");
-            emailService.sendEmail(userEmail, USER_REGISTERED_TITLE, USER_REGISTERED_MESSAGE);
-            log.info("Email sent successfully!");
+            emailService.sendEmail(emailData);
         }
 
         var afterLoginPageUrl = urlsProperties.getAfterLoginPageUrl();
@@ -122,11 +141,16 @@ public class AccountController {
         log.info("Procedure finished!");
 
         String resetLink = urlsProperties.getResetPasswordUrl() + resetToken.getToken().toString();
-        String message = "Click this link to reset password: <a href='" + resetLink + "'>New password</a>";
+
+        EmailData emailData = EmailData.builder()
+                .receiver(email)
+                .title(EmailTitle.PASSWORD_RESET.getTitle())
+                .template(EmailTemplate.FORGOT_PASSWORD.getTemplateName())
+                .variables(Map.of(EmailVariable.USERNAME.getVariable(), user.getUsername(), EmailVariable.RESET_LINK.getVariable(), resetLink))
+                .build();
 
         log.info("Sending email...");
-        emailService.sendEmail(email, "Restart password", message);
-        log.info("Email sent successfully!");
+        emailService.sendEmail(emailData);
 
         return ResponseEntity
                 .status(HttpStatus.OK)
