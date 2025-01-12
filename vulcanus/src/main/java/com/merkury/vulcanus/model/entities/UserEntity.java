14c1b82e63820f92f10fb72cc1516e5fd86500c9
Adam Langmesser <s27119@pjwstk.edu.pl>
2025-01-12 13:37:42 +0100

    MER-168 Podbicie wersji mongodb do testów, poprawa relacji w UserEntity oraz poprawa testów endpointów.

--- vulcanus/src/main/java/com/merkury/vulcanus/model/entities/UserEntity.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/entities/UserEntity.java
@@ -2,8 +2,22 @@ package com.merkury.vulcanus.model.entities;
 
 import com.merkury.vulcanus.model.enums.Provider;
 import com.merkury.vulcanus.model.enums.UserRole;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -29,16 +43,19 @@ public class UserEntity implements UserDetails {
 
     /**
      * Default lazy loading: Images are not loaded immediately with the UserEntity.
-     * Implication: Accessing this collection outside an active transaction will throw a LazyInitializationException.
+     * Implication: Accessing this collection outside an active transaction will
+     * throw a LazyInitializationException.
      * Benefit: Improves performance by loading the collection only when needed.
      **/
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Img> images;
 
     /**
-     *  Default lazy loading: Favorite spots are loaded only when explicitly accessed.
-     *  Implication: Improper use of this collection outside a transaction can result in LazyInitializationException.
-     *  Reason: Avoids unnecessary fetching of potentially large collections unless required.
+     * Default lazy loading: Favorite spots are loaded only when explicitly accessed.
+     * Implication: Improper use of this collection outside a transaction can result
+     * in LazyInitializationException.
+     * Reason: Avoids unnecessary fetching of potentially large collections
+     * unless required.
      **/
     @ManyToMany
     @JoinTable(
@@ -56,11 +73,22 @@ public class UserEntity implements UserDetails {
 
     /**
      * Default lazy loading: Friendships are loaded on-demand.
-     * Implication: Direct access to this collection outside a transaction will result in LazyInitializationException.
-     * Purpose: Optimizes memory and database performance by avoiding eager loading of relationships.
+     * Implication: Direct access to this collection outside a transaction
+     * will result in LazyInitializationException.
+     * Purpose: Optimizes memory and database performance by avoiding eager
+     * loading of relationships.
      **/
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Friendship> friendships = new ArrayList<>();
+
+    /**
+     * Nowa relacja: Comments.
+     * Dzięki @OneToMany z cascade = ALL i orphanRemoval = true,
+     * przy usunięciu użytkownika skasują się także jego komentarze.
+     **/
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    @ToString.Exclude
+    private List<Comment> comments = new ArrayList<>();
 
     private Boolean accountNonExpired = true;
     private Boolean accountNonLocked = true;
