42783333fbe792663cfcfe26bc5e9a39194ea636
Halvah <bart.bohatyrewicz@gmail.com>
2025-01-20 09:06:36 +0100

    Refactored weather api call and fixed comment page buttons

--- venus/src/components/map/spot/Comments.jsx
+++ venus/src/components/map/spot/Comments.jsx
@@ -7,7 +7,7 @@ import { getComments } from "../../../http/comments.js";
 export default function Comments({ spotId, commentsPerPage = 2 }) {
   const [currentPage, setCurrentPage] = useState(0);
 
-  const { data, error } = useQuery({
+  const { data } = useQuery({
     queryFn: () => getComments(spotId, currentPage, commentsPerPage),
     queryKey: ["comments", spotId, currentPage, commentsPerPage],
     keepPreviousData: true,
@@ -45,10 +45,13 @@ export default function Comments({ spotId, commentsPerPage = 2 }) {
         onPageChange={handlePageClick}
         pageCount={data?.totalPages || 0}
         containerClassName="flex justify-center space-x-2 mt-1 text-sm"
-        pageClassName="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded cursor-pointer"
+        pageClassName="bg-gray-200 hover:bg-gray-300 rounded cursor-pointer"
+        pageLinkClassName="w-full h-full flex items-center justify-center px-2 py-1"
         activeClassName="bg-blue-300 text-black"
-        previousClassName="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded cursor-pointer"
-        nextClassName="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded cursor-pointer"
+        previousClassName="bg-gray-200 hover:bg-gray-300 rounded cursor-pointer"
+        previousLinkClassName="w-full h-full flex items-center justify-center px-2 py-1"
+        nextClassName="bg-gray-200 hover:bg-gray-300 rounded cursor-pointer"
+        nextLinkClassName="w-full h-full flex items-center justify-center px-2 py-1"
         disabledClassName="opacity-50 cursor-not-allowed"
       />
     </>
--- venus/src/http/weather.js
+++ venus/src/http/weather.js
@@ -1,91 +1,57 @@
-import { fetchWeatherApi } from "openmeteo";
+import axios from "axios";
 
 const fetchWeatherData = async (latitude, longitude) => {
-  const params = {
-    latitude: latitude,
-    longitude: longitude,
-    current: [
-      "temperature_2m",
-      "relative_humidity_2m",
-      "is_day",
-      "weather_code",
-    ],
-    hourly: [
-      "wind_speed_1000hPa",
-      "wind_speed_975hPa",
-      "wind_speed_950hPa",
-      "wind_speed_925hPa",
-      "wind_speed_900hPa",
-    ],
-    daily: ["sunrise", "sunset"],
-    wind_speed_unit: "ms",
+  const response = await axios.get(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude.toFixed(2)}&longitude=${longitude.toFixed(2)}&current=temperature_2m,relative_humidity_2m,is_day,weather_code&hourly=wind_speed_1000hPa,wind_speed_975hPa,wind_speed_950hPa,wind_speed_925hPa,wind_speed_900hPa&daily=sunrise,sunset&wind_speed_unit=ms&forecast_days=1`,
+  );
+
+  const responseData = response.data;
+
+  const current = responseData.current;
+  const hourly = responseData.hourly;
+  const daily = responseData.daily;
+  const currentHour = new Date().getUTCHours();
+  const flooredHour = Math.floor(currentHour);
+  const closestHourlyIndex = flooredHour % 24;
+  const sunrise = String(daily["sunrise"][0]).split("T")[1];
+  const sunset = String(daily["sunset"][0]).split("T")[1];
+
+  return {
+    current: {
+      temperature2m: current["temperature_2m"].toFixed(1),
+      relativeHumidity2m: current["relative_humidity_2m"],
+      isDay: current["is_day"],
+      type: current["weather_code"],
+    },
+    hourly: {
+      winds: [
+        {
+          height: 100,
+          speed: parseInt(hourly["wind_speed_1000hPa"][closestHourlyIndex]),
+        },
+        {
+          height: 300,
+          speed: parseInt(hourly["wind_speed_975hPa"][closestHourlyIndex]),
+        },
+        {
+          height: 500,
+          speed: parseInt(hourly["wind_speed_950hPa"][closestHourlyIndex]),
+        },
+        {
+          height: 800,
+          speed: parseInt(hourly["wind_speed_925hPa"][closestHourlyIndex]),
+        },
+        {
+          height: 1000,
+          speed: parseInt(hourly["wind_speed_900hPa"][closestHourlyIndex]),
+        },
+      ],
+    },
+    daily: {
+      sunrise: sunrise,
+      sunset: sunset,
+    },
   };
-
-  try {
-    const responses = await fetchWeatherApi(
-      "https://api.open-meteo.com/v1/forecast",
-      params,
-    );
-    const response = responses[0];
-
-    const current = response.current();
-    const hourly = response.hourly();
-    const daily = response.daily();
-    const currentHour = new Date().getUTCHours();
-    const flooredHour = Math.floor(currentHour);
-    const closestHourlyIndex =
-      flooredHour % hourly.variables(0).valuesArray().length;
-
-    return {
-      current: {
-        temperature2m: current.variables(0).value().toFixed(1),
-        relativeHumidity2m: current.variables(1).value(),
-        isDay: current.variables(2).values(),
-        type: current.variables(3).value(),
-      },
-      hourly: {
-        winds: [
-          {
-            height: 100,
-            speed: parseInt(
-              hourly.variables(0).valuesArray()[closestHourlyIndex],
-            ),
-          },
-          {
-            height: 300,
-            speed: parseInt(
-              hourly.variables(1).valuesArray()[closestHourlyIndex],
-            ),
-          },
-          {
-            height: 500,
-            speed: parseInt(
-              hourly.variables(2).valuesArray()[closestHourlyIndex],
-            ),
-          },
-          {
-            height: 800,
-            speed: parseInt(
-              hourly.variables(3).valuesArray()[closestHourlyIndex],
-            ),
-          },
-          {
-            height: 1000,
-            speed: parseInt(
-              hourly.variables(4).valuesArray()[closestHourlyIndex],
-            ),
-          },
-        ],
-      },
-      daily: {
-        sunrise: daily.variables(0).valuesArray(),
-        sunset: daily.variables(1).valuesArray(),
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching weather data:", error);
-    throw error;
-  }
 };
 
 export default fetchWeatherData;
