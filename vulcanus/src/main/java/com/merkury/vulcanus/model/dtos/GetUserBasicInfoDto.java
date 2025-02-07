55676c0fe1162cbf90fe6ba20c162794c01c084f
Mredosz <mredosz4@gmail.com>
2025-02-07 15:46:04 +0100

    Add more annotations

--- vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/GetUserBasicInfoDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/GetUserBasicInfoDto.java
@@ -1,10 +1,14 @@
 package com.merkury.vulcanus.model.dtos;
 
 import com.merkury.vulcanus.model.enums.Provider;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
-public record GetUserBasicInfoDto(
-        Long id,
-        String username,
-        Provider provider,
-        String email) {
+public record GetUserBasicInfoDto(@Positive(message = "ID must be a positive number.")
+                                  Long id,
+                                  @NotBlank(message = "Username cannot be empty.")
+                                  String username,
+                                  Provider provider,
+                                  @NotBlank(message = "Email cannot be empty.")
+                                  String email) {
 }
--- vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/user/UserEditDataDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/user/UserEditDataDto.java
@@ -1,6 +1,7 @@
 package com.merkury.vulcanus.model.dtos.user;
 
 import com.merkury.vulcanus.model.enums.Provider;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,10 +11,17 @@ public record UserEditDataDto(String password,
                               @Email(message = "Email must be valid.")
                               String email,
                               @NotBlank(message = "Username cannot be empty.")
-                              @Size(min=3,max=16, message = "Username must be between 3 and 16 characters long.")
+                              @Size(min = 3, max = 16, message = "Username must be between 3 and 16 characters long.")
                               String username,
                               Provider provider,
                               Boolean isPasswordChanged,
-                              String oldPassword
-) {
+                              String oldPassword) {
+    @AssertTrue(message = "Password cannot be empty when changing password.")
+    public boolean isPasswordValid() {
+        return !Boolean.TRUE.equals(isPasswordChanged) || (password != null && !password.trim().isEmpty());
+    }
+    @AssertTrue(message = "Old password cannot be empty when changing password.")
+    public boolean isOldPasswordValid() {
+        return !Boolean.TRUE.equals(isPasswordChanged) || (oldPassword != null && !oldPassword.trim().isEmpty());
+    }
 }
