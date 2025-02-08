324bbdf00f9517dc7723b047ec5732ea702471a9
stanoz <s.oziemczuk@gmail.com>
2025-02-08 18:36:52 +0100

    removed pagination implemented by Bartek

--- venus/src/components/map/spot/Comments.jsx
+++ venus/src/components/map/spot/Comments.jsx
@@ -1,26 +1,10 @@
-import { useState } from "react";
-import ReactPaginate from "react-paginate";
 import Comment from "./Comment.jsx";
-import { useQuery } from "@tanstack/react-query";
-import { getComments } from "../../../http/comments.js";
+import Error from "../../error/Error.jsx";
 
-export default function Comments({ spotId, commentsPerPage = 2 }) {
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const { data, error } = useQuery({
-    queryFn: () => getComments(spotId, currentPage, commentsPerPage),
-    queryKey: ["comments", spotId, currentPage, commentsPerPage],
-    keepPreviousData: true,
-    refetchOnWindowFocus: false,
-    staleTime: 1000 * 60 * 5,
-  });
-
-  const handlePageClick = (event) => {
-    setCurrentPage(event.selected);
-  };
-
+export default function Comments({ spotId }) {
   return (
     <>
+      {error && <Error error={error} />}
       {data && data?.content.length >= 0 ? (
         <div className="border-2 border-neutral-200 px-2.5 py-1 rounded-sm">
           <p className="text-lg">Comments:</p>
@@ -35,22 +19,6 @@ export default function Comments({ spotId, commentsPerPage = 2 }) {
       ) : (
         <span>There are no comments!</span>
       )}
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel=">"
-        previousLabel="<"
-        initialPage={0}
-        pageRangeDisplayed={1}
-        marginPagesDisplayed={1}
-        onPageChange={handlePageClick}
-        pageCount={data?.totalPages || 0}
-        containerClassName="flex justify-center space-x-2 mt-1 text-sm"
-        pageClassName="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded cursor-pointer"
-        activeClassName="bg-blue-300 text-black"
-        previousClassName="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded cursor-pointer"
-        nextClassName="bg-gray-200 hover:bg-gray-300 px-2 py-1 rounded cursor-pointer"
-        disabledClassName="opacity-50 cursor-not-allowed"
-      />
     </>
   );
 }
--- venus/src/http/comments.js
+++ venus/src/http/comments.js
@@ -1,19 +1,2 @@
 import axios from "axios";
 import BASE_URL from "./base-url.js";
-
-export async function addComment({ text, spotId }) {
-  await axios.post(
-    `${BASE_URL}/spot/comment`,
-    { text, spotId },
-    {
-      withCredentials: true,
-    },
-  );
-}
-
-export async function getComments(spotId, page, size) {
-  const response = await axios.get(`${BASE_URL}/public/spot/comments`, {
-    params: { spotId, page, size },
-  });
-  return response.data;
-}
