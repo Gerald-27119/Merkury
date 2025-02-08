ef9c172440f5010dfbcdff11d27d17d01db4880a
stanoz <s.oziemczuk@gmail.com>
2025-02-08 12:27:43 +0100

    refactor to one return statement

--- venus/src/components/map/spot/Photo.jsx
+++ venus/src/components/map/spot/Photo.jsx
@@ -1,15 +1,11 @@
 export default function Photo({ photo, onClick = () => {}, ...props }) {
-  if (!photo) {
-    return (
-      <div className="w-fit h-fit">
-        <p>No photo to display</p>
-      </div>
-    );
-  }
-
   return (
-    <div onClick={onClick} className="w-fit h-fit">
-      <img {...props} src={photo.img} alt={photo.title} />
+    <div className="w-fit h-fit" onClick={photo ? onClick : undefined}>
+      {photo ? (
+        <img {...props} src={photo.img} alt={photo.title} />
+      ) : (
+        <p>No photo to display</p>
+      )}
     </div>
   );
 }
