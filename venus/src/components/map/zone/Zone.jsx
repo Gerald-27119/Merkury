2cc345259a5d04fa0a8e23ecce774ee093b1473e
stanoz <s.oziemczuk@gmail.com>
2025-02-08 15:11:55 +0100

    docs refactor

--- venus/src/components/map/zone/Zone.jsx
+++ venus/src/components/map/zone/Zone.jsx
@@ -10,7 +10,6 @@ import { spotDetailsModalAction } from "../../../redux/spot-modal.jsx";
  * @param {Object} props - Właściwości przekazywane do komponentu.
  * @param {Object} props.zone - Obiekt strefy (np.: spota dla droniarzy lub strefy ograniczonej przepisami)
  * @param {string} props.color - Kolor obramowania wielokąta.
- * @param {React.ComponentType} props.DetailsComponent - Komponent renderujący szczegóły strefy w oknie Popup.
  * @returns {JSX.Element} Element JSX przedstawiający strefę na mapie.
  */
 export default function Zone({ zone, color }) {
