3460e4a6fb13f7ca83ff1c3dbf67da32a71db025
stanoz <s.oziemczuk@gmail.com>
2025-02-08 12:17:56 +0100

    constants with tailwind classes for buttons moved above the component

--- venus/src/pages/account/Account.jsx
+++ venus/src/pages/account/Account.jsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 import Button from "./Button.jsx";
 import { useSelector } from "react-redux";
 
+const buttonClasses =
+  "text-4xl p-3 bg-gradient-to-r from-purple-500  to-teal-500 hover:to-purple-600  hover:from-teal-600 duration-600 justify-center rounded text-center align-middle leading-normal text-fuchsia-50 transition-all delay-200 ease-in-out";
+
 export default function Account() {
   const isLogged = useSelector((state) => state.account.isLogged);
 
-  const buttonClasses =
-    "text-4xl p-3 bg-gradient-to-r from-purple-500  to-teal-500 hover:to-purple-600  hover:from-teal-600 duration-600 justify-center rounded text-center align-middle leading-normal text-fuchsia-50 transition-all delay-200 ease-in-out";
   return (
     <div className="bg-[url('/bg-kapi.png')] bg-cover bg-center bg-no-repeat flex h-screen w-full items-center justify-center space-x-8">
       {!isLogged && (
