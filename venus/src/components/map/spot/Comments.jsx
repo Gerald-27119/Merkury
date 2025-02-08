32a330227c5e2430c1275ec79cf5cdd4dc1cbeba
stanoz <s.oziemczuk@gmail.com>
2025-02-08 19:02:31 +0100

    implemented pagination in comments

--- venus/src/components/map/spot/Comments.jsx
+++ venus/src/components/map/spot/Comments.jsx
@@ -1,11 +1,31 @@
 import Comment from "./Comment.jsx";
 import Error from "../../error/Error.jsx";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getPaginatedComments } from "../../../http/comments.js";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
 
 export default function Comments({ spotId }) {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["spot", "comments", spotId],
+    queryFn: () => getPaginatedComments(spotId, currentPage, 2),
+    keepPreviousData: true,
+    staleTime: 10 * 60 * 1000,
+  });
+
+  const handlePageChange = (event) => {
+    setCurrentPage(event.selected);
+    queryClient.invalidateQueries(["spot", "comments", spotId]);
+  };
+
   return (
     <>
       {error && <Error error={error} />}
-      {data && data?.content.length >= 0 ? (
+      {data && data.content.length >= 0 ? (
         <div className="border-2 border-neutral-200 px-2.5 py-1 rounded-sm">
           <p className="text-lg">Comments:</p>
           <ul>
@@ -15,6 +35,22 @@ export default function Comments({ spotId }) {
               </li>
             ))}
           </ul>
+          <ReactPaginate
+            pageCount={data.totalPages}
+            previousLabel={"<"}
+            nextLabel={">"}
+            breakLabel={"..."}
+            initialPage={currentPage}
+            marginPagesDisplayed={1}
+            pageRangeDisplayed={1}
+            onPageChange={handlePageChange}
+            containerClassName="flex justify-center items-center space-x-2 mt-3 mb-1"
+            pageLinkClassName="px-2 py-1 text-black bg-white border border-gray-300 rounded-md hover:bg-gray-200 hover:text-gray-900"
+            activeLinkClassName="!bg-blue-500 !text-white !border-blue-500"
+            nextLinkClassName="px-2 py-1 text-black bg-white border border-gray-300 rounded-md hover:bg-gray-200 hover:text-gray-900"
+            previousLinkClassName="px-2 py-1 text-black bg-white border border-gray-300 rounded-md hover:bg-gray-200 hover:text-gray-900"
+            disabledLinkClassName="opacity-50 cursor-not-allowed"
+          />
         </div>
       ) : (
         <span>There are no comments!</span>
--- venus/src/http/comments.js
+++ venus/src/http/comments.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 import BASE_URL from "./base-url.js";
+
+export async function getPaginatedComments(spotId, page, commentsPerPage) {
+  return (
+    await axios.get(
+      `${BASE_URL}/public/spot/comments?spotId=${spotId}&page=${page}&size=${commentsPerPage}`,
+    )
+  ).data;
+}
