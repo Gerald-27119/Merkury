03cfeb8f816f3f41b4f8b3b382826f9e83b7f5c1
Mredosz <115088433+Mredosz@users.noreply.github.com>
2025-02-09 20:25:52 +0100

    feature/MER-176 (#67)
    * add auto logout when user refresh page
    
    * add endpoint

--- venus/src/pages/forum/Forum.jsx
+++ venus/src/pages/forum/Forum.jsx
@@ -1,19 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { test } from "../../http/account.js";
-import JwtError from "../../components/error/JwtError.jsx";
 
 export default function Forum() {
-  const { error, isError, isLoading } = useQuery({
+  const { isLoading } = useQuery({
     queryKey: ["test"],
     queryFn: test,
   });
 
   if (isLoading) {
     return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <JwtError error={error} />;
   }
 
   return (
