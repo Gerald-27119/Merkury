5816424e46fc1d238f77b84a26851bad8372075c
Mredosz <mredosz4@gmail.com>
2025-02-10 14:55:40 +0100

    change rating min validation to 0

--- vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/comment/CommentDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/comment/CommentDto.java
@@ -8,7 +8,7 @@ public record CommentDto(@Positive(message = "ID must be a positive number.")
                          Long id,
                          @NotBlank(message = "Text cannot be empty.")
                          String text,
-                         @Min(value = 1, message = "Rating count cannot be less than 1.")
+                         @Min(value = 0, message = "Rating count cannot be less than 0.")
                          @Max(value = 5, message = "Rating count cannot be more than 5.")
                          Double rating,
                          @Min(value = 0, message = "Likes cannot be less than 0.")
