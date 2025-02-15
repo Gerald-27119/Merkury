a10911a16b28765e5ecc79006e9572dbade7ebca
Mredosz <mredosz4@gmail.com>
2025-02-15 18:21:41 +0100

    test java

--- vulcanus/src/main/java/com/merkury/vulcanus/test/TestController.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/test/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @InvocationsCounter
 public class TestController {
-//testtttt
+//testttttt
     private final TestService testService;
 
     @GetMapping("/public/test")
