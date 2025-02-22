ea2240042269d3f2c1e6fbd95afff84d5d37f9ea
stanoz <s.oziemczuk@gmail.com>
2025-02-22 21:15:16 +0100

    corrected the test

--- vulcanus/src/test/java/com/merkury/vulcanus/controllers/spot/SpotControllerWithServerStartupTest.java
+++ vulcanus/src/test/java/com/merkury/vulcanus/controllers/spot/SpotControllerWithServerStartupTest.java
@@ -1,25 +1,27 @@
 package com.merkury.vulcanus.controllers.spot;
 
-import com.merkury.vulcanus.exception.exceptions.SpotsNotFoundException;
-import com.merkury.vulcanus.features.spot.SpotService;
 import com.merkury.vulcanus.model.dtos.spot.GeneralSpotDto;
+import com.merkury.vulcanus.model.embeddable.BorderPoint;
+import com.merkury.vulcanus.model.entities.Spot;
 import com.merkury.vulcanus.model.repositories.SpotRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,37 +36,49 @@ public class SpotControllerWithServerStartupTest {
     @Autowired
     private SpotRepository spotRepository;
 
-    @MockBean
-    private SpotService spotService;
+    @BeforeEach
+    void setUp() {
+        var borderPoints1 = List.of(
+                new BorderPoint(40.785091, -73.968285),
+                new BorderPoint(40.784091, -73.969285)
+        );
+
+        var borderPoints2 = List.of(
+                new BorderPoint(37.769420, -122.486214),
+                new BorderPoint(37.768420, -122.487214)
+        );
+        var spot1 = Spot.builder()
+                .name("Spot1")
+                .rating(3.0)
+                .areaColor("#000000")
+                .borderPoints(borderPoints1)
+                .build();
+
+        var spot2 = Spot.builder()
+                .name("Spot2")
+                .rating(4.5)
+                .areaColor("#000000")
+                .borderPoints(borderPoints2)
+                .build();
+
+        spotRepository.deleteAll();
+        spotRepository.save(spot1);
+        spotRepository.save(spot2);
+    }
 
     @Test
     @DisplayName("Filter spots should return all spots when no filters are set and spots are found.")
-    void filterSpotsWithNoFiltersReturnsAllSpotsWhenSpotsFound() throws SpotsNotFoundException {
+    void filterSpotsWithNoFiltersReturnsAllSpotsWhenSpotsFound() {
 
-        List<GeneralSpotDto> dummySpots = List.of(
-                GeneralSpotDto.builder()
-                        .id(1L)
-                        .name("Central Park")
-                        .rating(3.0)
-                        .areaColor("#00FF00")
-                        .build(),
-                GeneralSpotDto.builder()
-                        .id(2L)
-                        .name("Golden Gate Park")
-                        .rating(4.5)
-                        .areaColor("#FFD700")
-                        .build()
-        );
-
-        when(spotService.getFilteredSpots("", 0.0, 5.0)).thenReturn(dummySpots);
-
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
 
         var responseEntity = restTemplate.exchange(
                 "http://localhost:" + port + "/public/spot/filter",
                 HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<GeneralSpotDto>>() {
-                }
+                entity,
+                new ParameterizedTypeReference<List<GeneralSpotDto>>() {}
         );
 
         assertAll("Response Assertions",
