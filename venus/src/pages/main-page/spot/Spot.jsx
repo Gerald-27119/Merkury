333ee8f5e8a1d2497d9a0658d50003eec62e3cf8
stanoz <s.oziemczuk@gmail.com>
2025-03-15 21:32:39 +0100

    implemented design of the spot tile

--- venus/src/pages/main-page/spot/Spot.jsx
+++ venus/src/pages/main-page/spot/Spot.jsx
@@ -1,5 +1,6 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { Rate } from "antd";
+import { RiArrowDownSFill } from "react-icons/ri";
 
 export default function Spot({ spot }) {
   return (
@@ -14,12 +15,25 @@ export default function Spot({ spot }) {
           alt={spot.name}
           className="w-80 h-72 rounded-bl-2xl"
         />
-        <div className="flex-col ml-4">
-          <div className="custom-rate">
-            <Rate disabled allowHalf value={spot.rating} />
+        <div className="h-72 flex-col flex justify-between">
+          <div className="flex pr-6">
+            <div className="flex-col ml-4">
+              <div className="custom-rate">
+                <Rate disabled allowHalf value={spot.rating} />
+              </div>
+              <h2 className="text-xl max-w-36">{spot.name}</h2>
+              <span className="block w-72">{spot.description}</span>
+            </div>
+            <ul className="list-disc text-left ml-4">
+              <h3 className="text-lg">Tags:</h3>
+              {spot.categories.map((category) => (
+                <li key={category}>{category}</li>
+              ))}
+            </ul>
           </div>
-          <h2 className="text-xl max-w-36">{spot.name}</h2>
-          <span className="block w-72">{spot.description}</span>
+          <button className="flex items-center border-2 rounded-md w-fit mx-auto mb-3.5 px-1.5 hover:bg-gray-800 hover:border-zinc-600 hover:text-zinc-300">
+            Read more <RiArrowDownSFill size={35} />
+          </button>
         </div>
       </div>
     </div>
