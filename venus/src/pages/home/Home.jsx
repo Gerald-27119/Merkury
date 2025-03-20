3208c8c1100a2a7cc48412164bb2c958154fe3cc
Mredosz <mredosz4@gmail.com>
2025-03-20 13:41:20 +0100

    Add searchbar to advance search page

--- venus/src/pages/home/Home.jsx
+++ venus/src/pages/home/Home.jsx
@@ -11,6 +11,7 @@ import {
 } from "../../http/spots-data.js";
 import { useSelector } from "react-redux";
 import { FaLocationDot } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
 
 export default function Home({
   isAdvance,
@@ -35,27 +36,27 @@ export default function Home({
 
   return (
     <div className="min-h-screen w-full flex text-darkText">
-      <div className="w-1/4 bg-darkBgSoft flex flex-col items-center space-y-4 px-4">
-        <h1 className="text-3xl uppercase font-semibold text-center mt-3">
-          Filters
-        </h1>
-        <Select />
-        <SearchBar />
-        <div className="flex flex-col w-full items-center space-y-1">
-          <h1 className="font-semibold text-lg">Rating</h1>
-          <div className="flex justify-around bg-darkBgMuted pb-2 pt-1 rounded-md w-full">
-            <Stars value={0} text="from" />
-            <Stars value={4} text="to" />
-          </div>
-        </div>
-        <div className="flex flex-col w-full">
-          <TagCheckbox number={1} />
-          <TagCheckbox number={2} />
-          <TagCheckbox number={3} />
-        </div>
-        <Range />
-      </div>
-      <div className="w-3/4 bg-darkBg flex flex-col items-center space-y-4 p-3 ">
+      {/*<div className="w-1/4 bg-darkBgSoft flex flex-col items-center space-y-4 px-4">*/}
+      {/*  <h1 className="text-3xl uppercase font-semibold text-center mt-3">*/}
+      {/*    Filters*/}
+      {/*  </h1>*/}
+      {/*  <Select />*/}
+      {/*  <SearchBar />*/}
+      {/*  <div className="flex flex-col w-full items-center space-y-1">*/}
+      {/*    <h1 className="font-semibold text-lg">Rating</h1>*/}
+      {/*    <div className="flex justify-around bg-darkBgMuted pb-2 pt-1 rounded-md w-full">*/}
+      {/*      <Stars value={0} text="from" />*/}
+      {/*      <Stars value={4} text="to" />*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*  <div className="flex flex-col w-full">*/}
+      {/*    <TagCheckbox number={1} />*/}
+      {/*    <TagCheckbox number={2} />*/}
+      {/*    <TagCheckbox number={3} />*/}
+      {/*  </div>*/}
+      {/*  <Range />*/}
+      {/*</div>*/}
+      <div className="w-full bg-darkBg flex flex-col items-center space-y-4 p-3 ">
         <div className="flex text-white my-5">
           <button
             onClick={handleToSimpleSearch}
@@ -68,6 +69,25 @@ export default function Home({
             className={`${isAdvance && "bg-gray-700 "} px-2 py-1.5 rounded-r-xl`}
           >
             Advanced filters
+          </button>
+        </div>
+        <div className="w-1/2 flex justify-end bg-darkBgSoft rounded-lg p-2 items-center">
+          <div className="flex flex-col w-full justify-start">
+            <label htmlFor="search" className="ml-2">
+              Location
+            </label>
+            <input
+              className="w-full rounded-lg py-3 px-2 bg-darkBgSoft focus:ring-0 focus:outline-0"
+              id="search"
+              placeholder="Your city"
+              // onChange={handleSearchCity}
+            />
+          </div>
+          <button
+            className="px-4 h-fit py-3 rounded-md bg-darkBg"
+            onClick={handleToAdvanceSearch}
+          >
+            <FaSearch />
           </button>
         </div>
         <ul className="flex flex-col items-center space-y-4">
