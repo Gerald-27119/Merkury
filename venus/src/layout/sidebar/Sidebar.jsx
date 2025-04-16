4c095888c84b9d4796a79e6c54b018286cbb0e3e
Mredosz <mredosz4@gmail.com>
2025-04-16 15:51:26 +0200

    Delete unused else

--- venus/src/layout/sidebar/Sidebar.jsx
+++ venus/src/layout/sidebar/Sidebar.jsx
@@ -29,8 +29,6 @@ export default function Sidebar() {
   useEffect(() => {
     if (location.pathname.includes("account")) {
       setIsSidebarOpen(true);
-    } else {
-      setIsSidebarOpen(false);
     }
   }, [location]);
 
