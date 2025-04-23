218fa5249c0d84d313f3af448672892d85c5cd75
stanoz <s.oziemczuk@gmail.com>
2025-04-23 21:10:36 +0200

    implemented open free map with libre map

---
+++ venus/src/new-map/LibreMap.tsx
@@ -0,0 +1,49 @@
+import { Map, Source, Layer } from "@vis.gl/react-maplibre";
+import type { FeatureCollection } from "geojson";
+const geojson: FeatureCollection = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [18.64668, 54.3514],
+            [18.64668, 54.3534],
+            [18.64868, 54.3534],
+            [18.64868, 54.3514],
+            [18.64668, 54.3514],
+          ],
+        ],
+      },
+      properties: {},
+    },
+  ],
+};
+
+export default function LibreMap() {
+  return (
+    <Map
+      initialViewState={{
+        longitude: 18.647680062452974,
+        latitude: 54.35239970303622,
+        zoom: 15,
+      }}
+      style={{ width: "100vw", height: "100vh" }}
+      mapStyle="https://tiles.openfreemap.org/styles/positron"
+      attributionControl={false}
+    >
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer
+          id="polygon"
+          type="fill"
+          paint={{
+            "fill-color": "#007cbf",
+            "fill-opacity": 0.4,
+          }}
+        />
+      </Source>
+    </Map>
+  );
+}
