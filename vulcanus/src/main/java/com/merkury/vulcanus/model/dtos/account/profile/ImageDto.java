774524b46e96934f1c1fda299bed80c83b0f2542
Mredosz <mredosz4@gmail.com>
2025-04-26 16:28:44 +0200

    Remove unnecessary annotations

--- vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/account/profile/ImageDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/account/profile/ImageDto.java
@@ -1,18 +1,12 @@
 package com.merkury.vulcanus.model.dtos.account.profile;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
+
 import lombok.Builder;
 
 @Builder
-public record ImageDto(@NotBlank(message = "Src cannot be empty.")
-                       String src,
-                       @Positive(message = "HeartCount must be a positive number.")
+public record ImageDto(String src,
                        Integer heartsCount,
-                       @Positive(message = "ViewsCount must be a positive number.")
                        Integer viewsCount,
-                       @NotBlank(message = "Title cannot be empty.")
                        String title,
-                       @Positive(message = "Id must be a positive number.")
                        Long id) {
 }
