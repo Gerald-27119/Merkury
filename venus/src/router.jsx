4bbb83e91615b76db5c920d441f1694ad85777c9
Mredosz <mredosz4@gmail.com>
2025-04-27 09:26:53 +0200

    Remove unnecessary sign out function and add protectedRoute for profile

--- venus/src/router.jsx
+++ venus/src/router.jsx
@@ -31,7 +31,11 @@ const router = createBrowserRouter([
           },
           {
             path: "profile",
-            element: <Profile />,
+            element: (
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            ),
           },
         ],
       },
