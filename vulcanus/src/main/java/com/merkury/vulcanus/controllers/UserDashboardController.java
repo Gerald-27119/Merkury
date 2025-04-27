b172ffd533b4b9b8ef3a9d890d8b33cfab035ae5
Mredosz <mredosz4@gmail.com>
2025-04-27 10:15:21 +0200

    Add requestMapping to controller

--- vulcanus/src/main/java/com/merkury/vulcanus/controllers/UserDashboardController.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/controllers/UserDashboardController.java
@@ -7,17 +7,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@RequestMapping("/user-dashboard")
 @RequiredArgsConstructor
 public class UserDashboardController {
     private final UserDashboardService userDashboardService;
 
-    @GetMapping("/user-dashboard/profile/{username}")
+    @GetMapping("/profile/{username}")
     public ResponseEntity<UserProfileDto> getUserProfile(@PathVariable String username) throws UserNotFoundByUsernameException {
         var user = userDashboardService.getUserProfile(username);
         return ResponseEntity.status(HttpStatus.OK).body(user);
