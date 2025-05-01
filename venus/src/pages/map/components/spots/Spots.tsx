62c5a83fda5741b985a1b66730082bb5a66e2c96
stanoz <s.oziemczuk@gmail.com>
2025-05-01 19:44:14 +0200

    removed console.log

--- venus/src/pages/map/components/spots/Spots.tsx
+++ venus/src/pages/map/components/spots/Spots.tsx
@@ -26,7 +26,7 @@ export default function Spots() {
     refetchOnWindowFocus: false,
     staleTime: 1000 * 60 * 5,
   });
-  console.log(data);
+
   useEffect(() => {
     if ((error as AxiosError)?.response?.data) {
       dispatch(
