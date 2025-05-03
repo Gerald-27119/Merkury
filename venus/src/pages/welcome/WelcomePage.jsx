d04e0f7f8ce870b3b0826961c5ecfe42c9aa74ce
stanoz <s.oziemczuk@gmail.com>
2025-05-03 11:33:52 +0200

    removed unused code

--- venus/src/pages/welcome/WelcomePage.jsx
+++ venus/src/pages/welcome/WelcomePage.jsx
@@ -1,15 +1,8 @@
-import MapPage from "../map/MapPage.tsx";
-import SpotDetails from "../spot/SpotDetails.jsx";
-import { useSelector } from "react-redux";
-
 export default function WelcomePage() {
-  const showDetailsModal = useSelector((state) => state.spotDetails.showModal);
   return (
     <div className="relative h-full w-full">
       <div className="relative z-10">
         <h1 className="h-screen">Welcome Page</h1>
-        {/*{showDetailsModal && <SpotDetails />}*/}
-        {/*<MapPage />*/}
       </div>
     </div>
   );
