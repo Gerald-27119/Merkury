de573ed64cfdd502bdc29fd9b6238c6249a5e15c
Mredosz <mredosz4@gmail.com>
2025-05-13 10:43:46 +0200

    Repair tooltip

--- venus/src/layout/sidebar/components/Tooltip.tsx
+++ venus/src/layout/sidebar/components/Tooltip.tsx
@@ -9,14 +9,16 @@ interface TooltipProps {
 export default function Tooltip({ links, name }: TooltipProps) {
   return (
     <div className="bg-violetLight text-darkText absolute top-0 left-full z-50 ml-3.5 rounded-r-md pt-2 text-start text-base font-semibold whitespace-nowrap capitalize">
-      <p className={`${links?.length ? "cursor-auto" : "cursor-pointer"} px-3`}>
+      <p
+        className={`${links?.length ? "cursor-auto" : "cursor-pointer"} px-3 pb-2`}
+      >
         {name}
       </p>
       {links?.map((link) => (
         <NavLink
           key={link.name}
           to={link.to}
-          className="hover:bg-violetDark block px-3 font-normal text-gray-300 last:rounded-br-md last:pb-2"
+          className="hover:bg-violetDark block px-3 pb-1 font-normal text-gray-300 last:rounded-br-md"
         >
           {link.name}
         </NavLink>
