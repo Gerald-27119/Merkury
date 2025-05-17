adc55f3644581f69355c2c7bf1ecac9cbd4dbdfb
Mredosz <mredosz4@gmail.com>
2025-05-17 17:20:55 +0200

    Fix color

--- venus/src/components/oauth/OauthButton.tsx
+++ venus/src/components/oauth/OauthButton.tsx
@@ -9,7 +9,7 @@ export default function OauthButton({ children, onClick }: OauthButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="mt-2 flex w-full cursor-pointer items-center rounded-md bg-gray-600 p-3 text-sm break-words text-white shadow-md shadow-gray-800/20 transition-all duration-300 hover:bg-gray-700 sm:text-lg dark:bg-gray-700 dark:shadow-black/50 dark:hover:bg-gray-800"
+      className="mt-2 flex w-full cursor-pointer items-center rounded-md bg-gray-700/90 p-3 text-sm break-words text-white shadow-md shadow-gray-800/20 transition-all duration-300 hover:bg-gray-700 sm:text-lg dark:bg-gray-800 dark:shadow-black/50 dark:hover:bg-gray-800/50"
     >
       {children}
     </button>
