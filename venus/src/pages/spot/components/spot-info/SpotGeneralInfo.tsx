b5c0d1f4eee28876567ed3bfa70b496d8dc3ce58
stanoz <s.oziemczuk@gmail.com>
2025-05-22 15:20:53 +0200

    refactor SpotGeneralInfo layout for better responsiveness

--- venus/src/pages/spot/components/spot-info/SpotGeneralInfo.tsx
+++ venus/src/pages/spot/components/spot-info/SpotGeneralInfo.tsx
@@ -19,7 +19,7 @@ export default function SpotGeneralInfo({
 }: SpotGeneralInfoProps) {
   return (
     <div className="mt-5 mb-2 flex-col space-y-6">
-      <div className="flex flex-col items-center space-y-2 xl:justify-between">
+      <div className="flex flex-col items-center space-y-2 xl:flex-row xl:justify-between">
         <p className="text-2xl">{name}</p>
         <div className="flex justify-end space-x-2">
           <div className="custom-rate text-ratingStarColor mx-2 mt-1 inline-flex min-w-fit">
