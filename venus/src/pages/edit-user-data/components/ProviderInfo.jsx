556122442c73b9368dd032c4adc48f4b831df483
Mredosz <115088433+Mredosz@users.noreply.github.com>
2025-05-23 12:57:14 +0200

    fix/MER-247 (#110)
    * Start working on login and register
    
    * Add more styling
    
    * Add animation to error
    
    * Add responsiveness
    
    * Add tests
    
    * Add tests
    
    * Fix color
    
    * Add tests
    
    * Add tests
    
    * Change after review

--- venus/src/pages/edit-user-data/components/ProviderInfo.jsx
+++ venus/src/pages/edit-user-data/components/ProviderInfo.jsx
@@ -2,8 +2,8 @@ import { getOauth2ProviderIcon } from "../../../utils/account.jsx";
 
 export default function ProviderInfo({ provider, email }) {
   return (
-    <div className="flex flex-col items-center bg-amber-50 p-3 rounded-md text-lg text-center">
-      <div className="flex space-x-3 mb-2">
+    <div className="flex flex-col items-center rounded-md bg-amber-50 p-3 text-center text-lg">
+      <div className="mb-2 flex space-x-3">
         <p>Your account was created via {provider}</p>
         {getOauth2ProviderIcon(provider)}
       </div>
