3020cd8b3a7e8d9bbcc2da73ac0aca88c5017a0a
KacperBadek <k.badek2001@gmail.com>
2025-05-28 14:15:18 +0200

    MER-239 changed search bar outline

--- venus/src/pages/forum/components/PostSearchBar.tsx
+++ venus/src/pages/forum/components/PostSearchBar.tsx
@@ -2,9 +2,9 @@ import { HiMenu } from "react-icons/hi";
 
 export default function PostSearchBar({}) {
   return (
-    <div className="bg-darkBgSoft flex cursor-pointer items-center gap-2 rounded-2xl p-2">
-      <input placeholder="Search"></input>
-      <HiMenu className="text-2xl" />
+    <div className="bg-darkBgSoft flex items-center gap-2 rounded-2xl p-2">
+      <input className="focus:outline-none" placeholder="Search"></input>
+      <HiMenu className="cursor-pointer text-2xl" />
     </div>
   );
 }
