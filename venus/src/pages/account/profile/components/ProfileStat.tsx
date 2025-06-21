65c2aaaec1d5d3b8f10483afe2544d0791a7c0bb
Mredosz <mredosz4@gmail.com>
2025-06-21 12:35:11 +0200

    Add button type

--- venus/src/pages/account/profile/components/ProfileStat.tsx
+++ venus/src/pages/account/profile/components/ProfileStat.tsx
@@ -13,6 +13,7 @@ export default function ProfileStat({
     <button
       className="flex cursor-pointer flex-col items-center gap-3"
       onClick={onClick}
+      type="button"
     >
       <p className="text-darkBorder text-2xl">{label}</p>
       <p className="group-hover:text-shadow-darkText text-4xl">{value ?? 0}</p>
--- venus/src/pages/account/social/components/SocialButton.tsx
+++ venus/src/pages/account/social/components/SocialButton.tsx
@@ -15,6 +15,7 @@ export default function SocialButton({
     <button
       className={`dark:bg-violetDark dark:hover:bg-violetLight bg-violetLight hover:bg-violetLighter text-darkText flex w-full cursor-pointer items-center justify-center rounded-md py-1.5 transition-all duration-300 ${isActive && "dark:bg-violetLight bg-violetLighter"}`}
       onClick={onClick}
+      type="button"
     >
       {children}
     </button>
