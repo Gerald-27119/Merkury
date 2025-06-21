88273c02e23ffbcae7700de0295e0ecf6cfb6109
Mredosz <mredosz4@gmail.com>
2025-06-21 14:08:19 +0200

    Add loading state and enable queries based on social type

--- venus/src/pages/account/social/SocialForViewer.tsx
+++ venus/src/pages/account/social/SocialForViewer.tsx
@@ -7,23 +7,29 @@ import {
 import Social from "./Social";
 import { useParams } from "react-router-dom";
 import LoadingSpinner from "../../../components/loading-spinner/LoadingSpinner";
+import useSelectorTyped from "../../../hooks/useSelectorTyped";
+import { SocialListType } from "../../../model/enum/account/social/socialListType";
 
 export default function SocialForViewer() {
   const { username } = useParams();
+  const type = useSelectorTyped((state) => state.social.type);
 
   const { data: friends, isLoading: isLoadingFriends } = useQuery({
     queryFn: () => getUserFriendsForViewer(username!),
     queryKey: ["friends", username],
+    enabled: type === SocialListType.FRIENDS,
   });
 
   const { data: followed, isLoading: isLoadingFollowed } = useQuery({
     queryFn: () => getUserFollowedForViewer(username!),
     queryKey: ["followed", username],
+    enabled: type === SocialListType.FOLLOWED,
   });
 
   const { data: followers, isLoading: isLoadingFollowers } = useQuery({
     queryFn: () => getUserFollowersForViewer(username!),
     queryKey: ["followers", username],
+    enabled: type === SocialListType.FOLLOWERS,
   });
 
   if (isLoadingFriends || isLoadingFollowed || isLoadingFollowers) {
--- venus/src/pages/account/social/UserOwnSocial.tsx
+++ venus/src/pages/account/social/UserOwnSocial.tsx
@@ -5,22 +5,34 @@ import {
   getUserOwnFollowers,
   getUserOwnFriends,
 } from "../../../http/user-dashboard";
+import useSelectorTyped from "../../../hooks/useSelectorTyped";
+import { SocialListType } from "../../../model/enum/account/social/socialListType";
+import LoadingSpinner from "../../../components/loading-spinner/LoadingSpinner";
 
 export default function UserOwnSocial() {
-  const { data: friends } = useQuery({
+  const type = useSelectorTyped((state) => state.social.type);
+
+  const { data: friends, isLoading: isLoadingFriends } = useQuery({
     queryFn: getUserOwnFriends,
     queryKey: ["friends"],
+    enabled: type === SocialListType.FRIENDS,
   });
 
-  const { data: followed } = useQuery({
+  const { data: followed, isLoading: isLoadingFollowed } = useQuery({
     queryFn: getUserOwnFollowed,
     queryKey: ["followed"],
+    enabled: type === SocialListType.FOLLOWED,
   });
 
-  const { data: followers } = useQuery({
+  const { data: followers, isLoading: isLoadingFollowers } = useQuery({
     queryFn: getUserOwnFollowers,
     queryKey: ["followers"],
+    enabled: type === SocialListType.FOLLOWERS,
   });
+
+  if (isLoadingFriends || isLoadingFollowed || isLoadingFollowers) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <Social
