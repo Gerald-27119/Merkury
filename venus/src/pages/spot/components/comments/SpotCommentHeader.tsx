af06d3b67718822724c99b0feedb6d967673ab12
stanoz <s.oziemczuk@gmail.com>
2025-06-22 12:27:45 +0200

    Update button styling in SpotCommentHeader for improved visibility

--- venus/src/pages/spot/components/comments/SpotCommentHeader.tsx
+++ venus/src/pages/spot/components/comments/SpotCommentHeader.tsx
@@ -5,7 +5,7 @@ export default function SpotCommentHeader() {
     <div className="w-full">
       <div className="mx-4 my-4 flex items-center justify-between">
         <h3 className="font-bold">Comments</h3>
-        <button className="bg-violetDarker ring-violetBtnBorderDark/50 dark:hover:bg-violetLight flex w-fit cursor-pointer items-center rounded-full px-3 py-2 text-xl ring-1">
+        <button className="bg-violetDarker ring-violetBtnBorderDark/50 dark:hover:bg-violetLight flex w-fit cursor-pointer items-center rounded-full px-3 py-2 text-xl ring-2">
           <FaPlus />
           <span className="ml-2">Add</span>
         </button>
