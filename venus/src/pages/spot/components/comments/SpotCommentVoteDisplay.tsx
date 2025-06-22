cffd2d8722baf8f400a9a151db43dac11005a106
stanoz <s.oziemczuk@gmail.com>
2025-06-22 12:26:05 +0200

    Enhance SpotCommentVoteDisplay to show vote counts and update SpotCommentVotesPanel to use new props

--- venus/src/pages/spot/components/comments/SpotCommentVoteDisplay.tsx
+++ venus/src/pages/spot/components/comments/SpotCommentVoteDisplay.tsx
@@ -2,10 +2,17 @@ import { ReactNode } from "react";
 
 type SpotCommentVoteDisplayProps = {
   children: ReactNode;
+  votes: number;
 };
 
 export default function SpotCommentVoteDisplay({
+  votes,
   children,
 }: SpotCommentVoteDisplayProps) {
-  return <div className="flex items-center space-x-1 text-2xl">{children}</div>;
+  return (
+    <div className="flex items-center space-x-1 text-2xl">
+      <div className="relative h-6 w-6">{children}</div>
+      <p>{votes}</p>
+    </div>
+  );
 }
--- venus/src/pages/spot/components/comments/SpotCommentVotesPanel.tsx
+++ venus/src/pages/spot/components/comments/SpotCommentVotesPanel.tsx
@@ -1,4 +1,4 @@
-import { BiLike } from "react-icons/bi";
+import { BiLike, BiSolidDislike, BiSolidLike } from "react-icons/bi";
 import { BiDislike } from "react-icons/bi";
 import SpotCommentVoteDisplay from "./SpotCommentVoteDisplay";
 
@@ -7,19 +7,24 @@ type SpotCommentVotesProps = {
   downvotes: number;
 };
 
+const inActiveBtnClassNames: string =
+  "absolute cursor-pointer transition-opacity hover:opacity-0";
+const activeBtnClassNames: string =
+  "absolute cursor-pointer opacity-0 transition-opacity hover:opacity-100";
+
 export default function SpotCommentVotesPanel({
   upvotes,
   downvotes,
 }: SpotCommentVotesProps) {
   return (
     <div className="mt-3 flex w-full justify-start space-x-3">
-      <SpotCommentVoteDisplay>
-        <BiLike className="cursor-pointer" />
-        <p>{upvotes}</p>
+      <SpotCommentVoteDisplay votes={upvotes}>
+        <BiLike className={inActiveBtnClassNames} />
+        <BiSolidLike className={activeBtnClassNames} />
       </SpotCommentVoteDisplay>
-      <SpotCommentVoteDisplay>
-        <BiDislike className="cursor-pointer" />
-        <p>{downvotes}</p>
+      <SpotCommentVoteDisplay votes={downvotes}>
+        <BiDislike className={inActiveBtnClassNames} />
+        <BiSolidDislike className={activeBtnClassNames} />
       </SpotCommentVoteDisplay>
     </div>
   );
