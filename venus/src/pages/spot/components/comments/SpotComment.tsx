c7d7dc1b105edd3aaa29191d0f850a39d8f03c93
stanoz <s.oziemczuk@gmail.com>
2025-06-24 15:12:26 +0200

    Remove unused state and commented-out code from SpotComment component

--- venus/src/pages/spot/components/comments/SpotComment.tsx
+++ venus/src/pages/spot/components/comments/SpotComment.tsx
@@ -1,14 +1,7 @@
 import { formatPublishDate } from "../../../../utils/spot-utils";
 import { Rate } from "antd";
-import { FaRegThumbsUp, FaRegThumbsDown, FaRegTrashCan } from "react-icons/fa6";
-import { FaRegEdit } from "react-icons/fa";
-import EditCommentForm from "./EditCommentForm.jsx";
-import { useState } from "react";
 import SpotCommentDto from "../../../../model/interface/spot/comment/spotCommentDto";
-import { FaPlus } from "react-icons/fa6";
-import Button from "../../../../components/buttons/Button";
 import SpotCommentAuthor from "./SpotCommentAuthor";
-import SpotCommentHeader from "./SpotCommentHeader";
 import SpotCommentVotesPanel from "./SpotCommentVotesPanel";
 import SpotCommentPhotos from "./SpotCommentPhotos";
 
@@ -17,39 +10,6 @@ type SpotCommentProps = {
 };
 
 export default function SpotComment({ comment }: SpotCommentProps) {
-  const [isEditing, setIsEditing] = useState(false);
-
-  // const handleEdit = (editedComment) => {
-  //   onEdit(comment.id, editedComment);
-  //   setIsEditing(false);
-  // };
-  //
-  // const triggerEdit = () => {
-  //   setIsEditing((prevState) => !prevState);
-  // };
-  //
-  // const handleDelete = () => {
-  //   onDelete(comment.id);
-  // };
-  //
-  // const handleVote = (isUpvote) => {
-  //   onVote(comment.id, isUpvote);
-  // };
-
-  // if (isEditing) {
-  //   return (
-  //     <div className="m-1 rounded-md border border-stone-400 p-2">
-  //       <div className="mb-2">
-  //         <EditCommentForm
-  //           comment={comment}
-  //           onSave={handleEdit}
-  //           onCancel={() => setIsEditing(false)}
-  //         />
-  //       </div>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="bg-second flex flex-col items-center rounded-2xl px-4 py-2">
       <div className="my-3 flex w-full flex-col items-start">
