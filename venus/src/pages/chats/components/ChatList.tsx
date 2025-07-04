002425bc5eeab21b49a2a290a1bafaffd4fea0bf
Adam Langmesser <114993042+Gerald-27119@users.noreply.github.com>
2025-07-04 13:43:10 +0200

    feature/MER-266 (#126)
    * feat: add formatting check to CI and update package.json scripts
    
    * feat: add formatting check to CI and update package.json scripts
    
    * test
    
    * style: standardize indentation across multiple files

--- venus/src/pages/chats/components/ChatList.tsx
+++ venus/src/pages/chats/components/ChatList.tsx
@@ -64,7 +64,7 @@ export default function ChatList() {
                 if (entry.isIntersecting) {
                     obs.unobserve(entry.target);
                     fetchNextPage();
-                }
+                } //test
             },
             { rootMargin: "200px" },
         );
