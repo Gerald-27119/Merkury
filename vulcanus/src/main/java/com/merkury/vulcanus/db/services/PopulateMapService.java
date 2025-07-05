8ac6e17ac5d3678f52db6dda84ad09e30a084fa5
Adam Langmesser <s27119@pjwstk.edu.pl>
2025-07-05 13:49:10 +0200

    Add import for Arrays in PopulateMapService

--- vulcanus/src/main/java/com/merkury/vulcanus/db/services/PopulateMapService.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/db/services/PopulateMapService.java
@@ -23,6 +23,7 @@ import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
