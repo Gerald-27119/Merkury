a6dc982bfadca7f22b46194f2b18238a0ec7d1a0
Adam Langmesser <114993042+s27119@users.noreply.github.com>
2025-07-17 15:57:49 +0200

    added todo comments

--- vulcanus/src/main/java/com/merkury/vulcanus/features/chat/gif/GifService.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/features/chat/gif/GifService.java
@@ -14,6 +14,9 @@ public class GifService {
 
     private final GifProviderClient client;
 
+//    TODO:czas przechwoania cahchea na 1h?
+//    zmiana configa web cleitna do requesto? bo obecnie cos jest w apce
+//    wogole webcleitn czy cyzm robic request do api
     @Cacheable(value = "gifSearch", key = "#query", unless = "#result == null || #result.block().isEmpty()")
     public Mono<List<GifDto>> search(String query) {
         return client.searchGifs(query, 20);
