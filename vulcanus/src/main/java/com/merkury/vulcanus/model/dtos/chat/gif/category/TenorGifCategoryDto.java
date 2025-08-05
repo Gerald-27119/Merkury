26860fd58db514195fb8f08d90c28cd3cac4a53b
stanoz <s.oziemczuk@gmail.com>
2025-08-05 21:50:21 +0200

    implement Serializable interface in TenorGifCategoryDto

--- vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/chat/gif/category/TenorGifCategoryDto.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/dtos/chat/gif/category/TenorGifCategoryDto.java
@@ -2,6 +2,11 @@ package com.merkury.vulcanus.model.dtos.chat.gif.category;
 
 import lombok.Builder;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Builder
-public record TenorGifCategoryDto(String searchTerm, String path, String gifUrl) {
+public record TenorGifCategoryDto(String searchTerm, String path, String gifUrl) implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 9181184545727856771L;
 }
