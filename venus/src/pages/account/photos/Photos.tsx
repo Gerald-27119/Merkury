bd51a1f146e88482d13f3001441544505894a435
Mredosz <mredosz4@gmail.com>
2025-08-09 13:30:33 +0200

    Implement infinite scrolling for user movies and photos

--- venus/src/pages/account/photos/Photos.tsx
+++ venus/src/pages/account/photos/Photos.tsx
@@ -1,8 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getSortedUserPhotos } from "../../../http/user-dashboard";
 import { AccountWrapperType } from "../../../model/enum/account/accountWrapperType";
 import { useDateSortFilter } from "../../../hooks/useDateSortFilter";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { notificationAction } from "../../../redux/notification";
 import useDispatchTyped from "../../../hooks/useDispatchTyped";
 import Media from "../components/Media";
@@ -11,12 +11,51 @@ export default function Photos() {
     const dispatch = useDispatchTyped();
     const { sortType, searchDate, handleSelectType, handleChangeDate } =
         useDateSortFilter();
+    const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
-    const { data, isLoading, isError } = useQuery({
+    const {
+        data,
+        isLoading,
+        isError,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+    } = useInfiniteQuery({
         queryKey: ["photos", sortType, searchDate.from, searchDate.to],
-        queryFn: () =>
-            getSortedUserPhotos(searchDate.from, searchDate.to, sortType),
+        queryFn: ({ pageParam = 0 }) =>
+            getSortedUserPhotos(
+                sortType,
+                searchDate.from,
+                searchDate.to,
+                pageParam,
+                12,
+            ),
+        getNextPageParam: (lastPage, allPages) =>
+            lastPage.hasNext ? allPages.length : undefined,
+        initialPageParam: 0,
     });
+
+    const allItems = data?.pages.flatMap((page) => page.items);
+
+    useEffect(() => {
+        if (!loadMoreRef.current) return;
+        const observer = new IntersectionObserver(
+            (entries) => {
+                if (
+                    entries[0].isIntersecting &&
+                    hasNextPage &&
+                    !isFetchingNextPage
+                ) {
+                    fetchNextPage();
+                }
+            },
+            { threshold: 1.0 },
+        );
+        observer.observe(loadMoreRef.current);
+        return () => {
+            observer.disconnect();
+        };
+    }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
     useEffect(() => {
         if (isError) {
@@ -36,7 +75,9 @@ export default function Photos() {
             onSortChange={handleSelectType}
             onDateChange={handleChangeDate}
             isLoading={isLoading}
-            mediaList={data}
+            mediaList={allItems}
+            loadMoreRef={loadMoreRef}
+            isFetchingNextPage={isFetchingNextPage}
         />
     );
 }
