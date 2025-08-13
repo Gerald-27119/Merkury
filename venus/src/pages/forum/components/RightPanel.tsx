3f6d23041f4a91b9b2fd1e684a7334743fed6c82
KacperBadek <k.badek2001@gmail.com>
2025-08-13 20:12:27 +0200

    MER-239 css cleanup

--- venus/src/pages/forum/components/RightPanel.tsx
+++ venus/src/pages/forum/components/RightPanel.tsx
@@ -4,7 +4,7 @@ export default function RightPanel({}) {
             <img
                 src="https://media.licdn.com/dms/image/v2/D4D03AQGSuE8lu6980Q/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1720270379931?e=2147483647&v=beta&t=WvUF7mBJcsvauestqsvY3cxgmuXfisj1yp-H9SQdmLA"
                 alt="photo"
-                className="mx-auto mt-6 rounded-lg p-1"
+                className="mx-auto mt-6 rounded-lg p-1 invert dark:invert-0"
             />
         </div>
     );
