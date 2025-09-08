b2710c47ce85ae65e5c0bdaf9bcf1bf524d8683b
stanoz <s.oziemczuk@gmail.com>
2025-09-08 20:32:51 +0200

    Add precipitation probability display to CustomTickLabel component

--- venus/src/pages/spot/components/weather/components/CustomTickLabel.tsx
+++ venus/src/pages/spot/components/weather/components/CustomTickLabel.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { VictoryLabel } from "victory";
 import { formatISOToAmPm } from "../../../../../utils/weather";
 import WeatherIcon from "../../../../map/components/weather/components/WeatherIcon";
+import { WiRaindrop } from "react-icons/wi";
 
 export default function CustomTickLabel(props) {
     const { index, x, y, data } = props;
     const time = formatISOToAmPm(data.at(index).time.toString());
     const temp = `${data.at(index).temperature}°C`;
-
+    const precipitationProbability = `${data.at(index).precipitationProbability}%`;
     return (
         <g transform={`translate(${x}, ${y})`}>
             <VictoryLabel
@@ -29,6 +30,18 @@ export default function CustomTickLabel(props) {
                 style={{ fontSize: 10, fill: "white" }}
                 dy={20}
             />
+            <g transform="translate(-17, 180)">
+                <WiRaindrop size={25} className="text-slate-400" />
+                <text
+                    x="20"
+                    y="17"
+                    fill="#94a3b8"
+                    fontSize="15"
+                    textAnchor="start"
+                >
+                    {precipitationProbability}
+                </text>
+            </g>
         </g>
     );
 }
