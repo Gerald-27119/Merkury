e1cceee8513fc8051153c0a2c006803627863271
Mredosz <115088433+Mredosz@users.noreply.github.com>
2025-09-14 21:57:16 +0200

    refactor/MER-290 (#164)
    * refactor: implement infinite scrolling for search results on home page
    
    * fix after review

---
+++ venus/src/model/interface/spot/search-spot/homePageSpotPageDto.ts
@@ -0,0 +1,6 @@
+import HomePageSpotDto from "./homePageSpotDto";
+
+export interface HomePageSpotPageDto {
+    items: HomePageSpotDto[];
+    hasNext: boolean;
+}
--- venus/src/pages/home-page/AdvanceHomePage.tsx
+++ venus/src/pages/home-page/AdvanceHomePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import SearchSpotList from "./components/SearchSpotList";
 import Switch from "./components/Switch";
 import AdvanceSearchBar from "./components/AdvanceSearchBar";
@@ -6,6 +6,9 @@ import HomePageSpotDto from "../../model/interface/spot/search-spot/homePageSpot
 
 export default function AdvanceHomePage() {
     const [searchedSpots, setSearchedSpots] = useState<HomePageSpotDto[]>([]);
+    const [isFetchingNextPage, setIsFetchingNextPage] = useState(false);
+
+    const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
     const handleSetSearchedSpots = (spots: HomePageSpotDto[]) => {
         setSearchedSpots(spots);
@@ -14,9 +17,17 @@ export default function AdvanceHomePage() {
     return (
         <div className="dark:bg-darkBg dark:text-darkText bg-lightBg text-lightText mt-10 flex min-h-screen w-full flex-col items-center space-y-4 overflow-hidden p-8">
             <Switch />
-            <AdvanceSearchBar onSetSpots={handleSetSearchedSpots} />
+            <AdvanceSearchBar
+                onSetSpots={handleSetSearchedSpots}
+                loadMoreRef={loadMoreRef}
+                onSetFetchingNextPage={setIsFetchingNextPage}
+            />
             <div className="flex w-full flex-col items-center space-y-10">
-                <SearchSpotList spots={searchedSpots} />
+                <SearchSpotList
+                    spots={searchedSpots}
+                    loadMoreRef={loadMoreRef}
+                    isFetchingNextPage={isFetchingNextPage}
+                />
             </div>
         </div>
     );
