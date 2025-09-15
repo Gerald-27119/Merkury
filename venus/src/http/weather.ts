3daa4334c90f5f649084a71650b0e7f395fb9632
stanoz <s.oziemczuk@gmail.com>
2025-09-15 21:39:09 +0200

    Remove unused weather data fetching and processing functions for cleaner code

--- venus/src/http/weather.ts
+++ venus/src/http/weather.ts
@@ -85,28 +85,3 @@ export async function getWeatherDataForTimelinePlot(
         })
     ).data;
 }
-
-export async function fetchWeather(latitude, longitude) {
-    return (
-        await axios.get("https://api.open-meteo.com/v1/forecast", {
-            params: {
-                latitude,
-                longitude,
-                current: ["temperature_2m", "weather_code"],
-                hourly: [
-                    "wind_speed_1000hPa",
-                    "wind_speed_975hPa",
-                    "wind_speed_950hPa",
-                    "wind_speed_925hPa",
-                    "wind_speed_900hPa",
-                    "wind_speed_850hPa",
-                    "wind_speed_800hPa",
-                    "wind_speed_10m",
-                    "wind_speed_80m",
-                ],
-                daily: ["sunrise", "sunset"],
-                wind_speed_unit: "ms",
-            },
-        })
-    ).data;
-}
--- venus/src/pages/spot/components/weather/WeatherRow.jsx
+++
@@ -1,5 +0,0 @@
-export default function WeatherRow({ children }) {
-    return (
-        <div className="flex flex-wrap justify-between gap-4">{children}</div>
-    );
-}
