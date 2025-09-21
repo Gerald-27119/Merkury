4b3bf4edea1723def3db075e9eb9a6869253a011
Adam Langmesser <114993042+s27119@users.noreply.github.com>
2025-09-21 10:03:58 +0200

    Update SocialMapper to include commonPrivateChatId in DTO conversions

--- vulcanus/src/main/java/com/merkury/vulcanus/model/mappers/user/dashboard/SocialMapper.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/model/mappers/user/dashboard/SocialMapper.java
@@ -11,10 +11,11 @@ public class SocialMapper {
     private SocialMapper() {
     }
 
-    public static SocialDto toDto(@NotNull Friendship friendship) {
+    public static SocialDto toDto(@NotNull Friendship friendship, @Nullable Long commonPrivateChatId) {
         return SocialDto.builder()
                 .username(friendship.getFriend().getUsername())
                 .profilePhoto(friendship.getFriend().getProfilePhoto())
+                .commonPrivateChatId(commonPrivateChatId)
                 .build();
     }
 
@@ -22,51 +23,15 @@ public class SocialMapper {
         return SocialDto.builder()
                 .username(userEntity.getUsername())
                 .profilePhoto(userEntity.getProfilePhoto())
-                .commonPrivateChatId(commonPrivateChatId)
+                .commonPrivateChatId(commonPrivateChatId)//TODO:sprawdz czy w miejscach wywolan to dziala i pozostale 2 metody
                 .build();
     }
 
-    public static SocialDto toDto(@NotNull FriendView friendView) {
+    public static SocialDto toDto(@NotNull FriendView friendView, @Nullable Long commonPrivateChatId) {
         return SocialDto.builder()
                 .username(friendView.getFriend().getUsername())
                 .profilePhoto(friendView.getFriend().getProfilePhoto())
+                .commonPrivateChatId(commonPrivateChatId)
                 .build();
     }
 }
-
-//package com.merkury.vulcanus.model.mappers.user.dashboard;
-//
-//import com.merkury.vulcanus.model.dtos.account.social.SocialDto;
-//import com.merkury.vulcanus.model.entities.Friendship;
-//import com.merkury.vulcanus.model.entities.UserEntity;
-//import com.merkury.vulcanus.model.interfaces.FriendView;
-//import jakarta.validation.constraints.NotNull;
-//
-//public class SocialMapper {
-//    private SocialMapper() {
-//    }
-//
-//    public static SocialDto toDto(@NotNull Friendship friendship, Long commonPrivateChatId) {
-//        return SocialDto.builder()
-//                .username(friendship.getFriend().getUsername())
-//                .profilePhoto(friendship.getFriend().getProfilePhoto())
-//                .commonPrivateChatId(commonPrivateChatId)
-//                .build();
-//    }
-//
-//    public static SocialDto toDto(@NotNull UserEntity userEntity, Long commonPrivateChatId) {
-//        return SocialDto.builder()
-//                .username(userEntity.getUsername())
-//                .profilePhoto(userEntity.getProfilePhoto())
-//                .commonPrivateChatId(commonPrivateChatId)
-//                .build();
-//    }
-//
-//    public static SocialDto toDto(@NotNull FriendView friendView, Long commonPrivateChatId) {
-//        return SocialDto.builder()
-//                .username(friendView.getFriend().getUsername())
-//                .profilePhoto(friendView.getFriend().getProfilePhoto())
-//                .commonPrivateChatId(commonPrivateChatId)
-//                .build();
-//    }
-//}
