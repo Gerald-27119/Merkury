0ead8fb3e6ad9161a2152c516e232fe37ebdfcc7
stanoz <s.oziemczuk@gmail.com>
2025-10-14 21:35:04 +0200

    feat: add share functionality to copy media URL to clipboard with notifications

--- venus/src/pages/spot/components/expanded-media-gallery/components/ExpandedMediaGalleryActions.tsx
+++ venus/src/pages/spot/components/expanded-media-gallery/components/ExpandedMediaGalleryActions.tsx
@@ -1,7 +1,9 @@
 import { AiOutlineShareAlt } from "react-icons/ai";
-import { IoClose } from "react-icons/io5";
 import useDispatchTyped from "../../../../../hooks/useDispatchTyped";
 import { expandedSpotMediaGalleryModalsActions } from "../../../../../redux/expanded-spot-media-gallery-modals";
+import { IoClose } from "react-icons/io5";
+import { notificationAction } from "../../../../../redux/notification";
+import useSelectorTyped from "../../../../../hooks/useSelectorTyped";
 
 export default function ExpandedMediaGalleryActions() {
     const dispatch = useDispatchTyped();
@@ -10,9 +12,39 @@ export default function ExpandedMediaGalleryActions() {
         dispatch(expandedSpotMediaGalleryModalsActions.closeModals());
     };
 
+    const mediaUrl = useSelectorTyped(
+        (state) => state.expandedSpotGalleryCurrentMedia.url,
+    );
+
+    const handleClickShare = async (mediaUrl: string) => {
+        if (!navigator.clipboard) {
+            dispatch(
+                notificationAction.addInfo({
+                    message: "Clipboard API not supported",
+                }),
+            );
+            return;
+        }
+        try {
+            await navigator.clipboard.writeText(mediaUrl);
+            dispatch(
+                notificationAction.addSuccess({
+                    message: "Media url copied to clipboard!",
+                }),
+            );
+        } catch (err) {
+            dispatch(
+                notificationAction.addError({ message: "Failed to copy: " }),
+            );
+        }
+    };
+
     return (
         <div className="flex items-center space-x-2">
-            <div className="flex cursor-pointer items-center space-x-1">
+            <div
+                className="hover:bg-grayBg/40 flex cursor-pointer items-center space-x-1 rounded-2xl py-0.5 pr-3 pl-2"
+                onClick={() => handleClickShare(mediaUrl)}
+            >
                 <AiOutlineShareAlt className="text-2xl" />
                 <span>Share</span>
             </div>
