5661ad244921319afa85f1348d8b56a1194b8d77
stanoz <s.oziemczuk@gmail.com>
2025-10-15 18:52:04 +0200

    feat: enhance fullscreen media modal layout and improve video display placeholder

--- venus/src/pages/map/components/spot-gallery/FullscreenMediaModal.tsx
+++ venus/src/pages/map/components/spot-gallery/FullscreenMediaModal.tsx
@@ -4,7 +4,7 @@ import useDispatchTyped from "../../../../hooks/useDispatchTyped";
 import { expandedSpotMediaGalleryFullscreenSizeActions } from "../../../../redux/expanded-spot-media-gallery-fullscreen-size";
 import { MediaType } from "../../../../model/enum/mediaType";
 
-//TODO add video display and animation center media
+//TODO add video display and animation
 export default function FullscreenMediaModal() {
     const { url, mediaType } = useSelectorTyped(
         (state) => state.expandedSpotGalleryCurrentMedia,
@@ -17,16 +17,22 @@ export default function FullscreenMediaModal() {
     };
 
     return (
-        <div className="absolute top-0 z-[6] flex h-full w-full flex-col items-end justify-center bg-black">
+        <div className="absolute top-0 z-[4] flex h-full w-full flex-col items-center bg-black">
             <IoClose
                 onClick={handleCloseFullscreenModal}
-                className="text-darkText cursor-pointer text-2xl"
+                className="text-darkText mt-2 ml-auto cursor-pointer text-2xl"
             />
-            {mediaType === MediaType.PHOTO ? (
-                <img alt={url} src={url} className="max-h-full max-w-full" />
-            ) : (
-                <p>video</p>
-            )}
+            <div className="flex h-full items-center">
+                {mediaType === MediaType.PHOTO ? (
+                    <img
+                        alt={url}
+                        src={url}
+                        className="max-h-full max-w-full"
+                    />
+                ) : (
+                    <p>video</p>
+                )}
+            </div>
         </div>
     );
 }
