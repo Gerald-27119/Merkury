d1c7dae02f5d619a1fe9584310c4ae8294b874a7
stanoz <s.oziemczuk@gmail.com>
2025-11-05 21:44:44 +0100

    Implement functionality to like and edit spot media likes

--- venus/src/pages/spot/components/expanded-media-gallery/components/expanded-media-display/ExpandedGalleryPanel.tsx
+++ venus/src/pages/spot/components/expanded-media-gallery/components/expanded-media-display/ExpandedGalleryPanel.tsx
@@ -1,8 +1,16 @@
 import { HiOutlineArrowsExpand } from "react-icons/hi";
 import { BiDownload } from "react-icons/bi";
-import { GoHeart } from "react-icons/go";
+import { GoHeart, GoHeartFill } from "react-icons/go";
 import useDispatchTyped from "../../../../../../hooks/useDispatchTyped";
 import { expandedSpotMediaGalleryFullscreenSizeActions } from "../../../../../../redux/expanded-spot-media-gallery-fullscreen-size";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+    checkIsSpotMediaLikedByUser,
+    editSpotMediaLikes,
+} from "../../../../../../http/user-dashboard";
+import useSelectorTyped from "../../../../../../hooks/useSelectorTyped";
+import { notificationAction } from "../../../../../../redux/notification";
+import { useEffect, useState } from "react";
 
 type ExpandedGalleryPanel = {
     url: string;
@@ -15,10 +23,77 @@ export default function ExpandedGalleryPanel({
     likes,
     spotMediaId,
 }: ExpandedGalleryPanel) {
+    const [likesCount, setLikesCount] = useState<number>(likes);
+    const [isSpotMediaLikedByUser, setIsSpotMediaLikedByUser] =
+        useState<boolean>(false);
+
     const dispatch = useDispatchTyped();
+    const queryClient = useQueryClient();
+
+    const { isLogged } = useSelectorTyped((state) => state.account);
+    const { mediaType, mediaId } = useSelectorTyped(
+        (state) => state.expandedSpotMediaGallery,
+    );
+    const { spotId } = useSelectorTyped((state) => state.spotDetails);
 
     const handleSetMediaFullscreen = () => {
         dispatch(expandedSpotMediaGalleryFullscreenSizeActions.setFullScreen());
+    };
+
+    const { mutateAsync } = useMutation({
+        mutationKey: ["edit-spot-media-likes", spotMediaId],
+        mutationFn: editSpotMediaLikes,
+        onError: () => {
+            dispatch(
+                notificationAction.addError({
+                    message: "Failed to edit spot media likes.",
+                }),
+            );
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ["is-spot-media-liked-by-user", spotMediaId],
+            });
+            await queryClient.invalidateQueries({
+                queryKey: [
+                    "expanded-media-display",
+                    mediaType,
+                    mediaId,
+                    spotId,
+                ],
+            });
+        },
+    });
+
+    const { data, isSuccess } = useQuery({
+        queryKey: ["is-spot-media-liked-by-user", spotMediaId],
+        queryFn: () => checkIsSpotMediaLikedByUser(spotMediaId),
+        enabled: isLogged,
+    });
+
+    useEffect(() => {
+        if (isSuccess && data) {
+            setIsSpotMediaLikedByUser(data.isSpotMediaLiked);
+        }
+    }, [isSuccess, data]);
+
+    const handleClickEditSpotMediaLikes = async () => {
+        if (!isLogged) {
+            dispatch(
+                notificationAction.addInfo({
+                    message: "Please sign in to like media.",
+                }),
+            );
+        } else {
+            if (isSpotMediaLikedByUser) {
+                setLikesCount((prevState) => prevState - 1);
+                setIsSpotMediaLikedByUser(false);
+            } else {
+                setLikesCount((prevState) => prevState + 1);
+                setIsSpotMediaLikedByUser(true);
+            }
+            await mutateAsync(spotMediaId);
+        }
     };
 
     return (
@@ -30,9 +105,16 @@ export default function ExpandedGalleryPanel({
             <a href={url}>
                 <BiDownload className="cursor-pointer" />
             </a>
-            <div className="flex items-center space-x-1">
-                <GoHeart className="cursor-pointer" />
-                <span>{likes}</span>
+            <div
+                onClick={handleClickEditSpotMediaLikes}
+                className="hover:bg-grayBg/20 flex cursor-pointer items-center space-x-1"
+            >
+                {isLogged && isSpotMediaLikedByUser ? (
+                    <GoHeartFill />
+                ) : (
+                    <GoHeart />
+                )}
+                <span>{likesCount}</span>
             </div>
         </div>
     );
