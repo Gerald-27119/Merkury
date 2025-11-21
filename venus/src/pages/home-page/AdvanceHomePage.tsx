6d9e043a06a2103a2acf36ad87e89eef171e229d
Mredosz <mredosz4@gmail.com>
2025-11-21 18:51:52 +0100

    End 6

--- venus/src/pages/home-page/AdvanceHomePage.tsx
+++ venus/src/pages/home-page/AdvanceHomePage.tsx
@@ -15,8 +15,7 @@ export default function AdvanceHomePage() {
     }, []);
 
     return (
-        <div className="dark:bg-darkBg dark:text-darkText bg-lightBg text-lightText
-        flex min-h-screen w-full flex-col items-center space-y-4 overflow-hidden p-8 pt-18">
+        <div className="dark:bg-darkBg dark:text-darkText bg-lightBg text-lightText flex min-h-screen w-full flex-col items-center space-y-4 overflow-hidden p-8 pt-18">
             <Switch />
             <AdvanceSearchBar
                 onSetSpots={handleSetSearchedSpots}
