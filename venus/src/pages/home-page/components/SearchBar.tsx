8e2fa2c45f3794cf2e9648925456c39ca2bde6ea
Mredosz <mredosz4@gmail.com>
2026-01-12 13:58:06 +0100

    Update queryKey in useInfiniteQuery to include searchLocation

--- venus/src/pages/home-page/components/SearchBar.tsx
+++ venus/src/pages/home-page/components/SearchBar.tsx
@@ -64,7 +64,7 @@ export default function SearchBar({
 
     const { fetchNextPage, hasNextPage, isFetchingNextPage, refetch } =
         useInfiniteQuery({
-            queryKey: ["homePageSpots"],
+            queryKey: ["homePageSpots", searchLocation],
             queryFn: ({ pageParam = 0 }) =>
                 getSearchedSpotsOnHomePage(
                     {
