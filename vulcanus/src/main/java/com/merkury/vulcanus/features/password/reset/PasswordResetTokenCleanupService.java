d6d50dab57db99ad28623ff8c66e5f231350e02d
KacperBadek <k.badek2001@gmail.com>
2026-01-13 01:56:17 +0100

    shedlock fixes

--- vulcanus/src/main/java/com/merkury/vulcanus/features/password/reset/PasswordResetTokenCleanupService.java
+++ vulcanus/src/main/java/com/merkury/vulcanus/features/password/reset/PasswordResetTokenCleanupService.java
@@ -1,6 +1,7 @@
 package com.merkury.vulcanus.features.password.reset;
 
 import com.merkury.vulcanus.model.repositories.PasswordResetTokenRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
@@ -17,13 +18,14 @@ public class PasswordResetTokenCleanupService {
     private final PasswordResetTokenRepository passwordResetTokenRepository;
 
     //jak sie nie da locka na krócej niż trwa rotacja schedulera to wywala błędy
+    @Transactional
     @Scheduled(cron = "0 */15 * * * *")
     @SchedulerLock(name = "cleanupExpiredTokens", lockAtMostFor = "29m", lockAtLeastFor = "14m")
     public void cleanupExpiredTokens() {
         LocalDateTime now = LocalDateTime.now();
 
         log.info("Cleaning up expired password reset tokens");
-        passwordResetTokenRepository.deleteByExpirationDateBefore(LocalDateTime.now());
+        passwordResetTokenRepository.deleteByExpirationDateBefore(now);
         System.out.println("Deleting password reset tokens...");
     }
 }
